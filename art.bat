c0ea15964b9f5c08d951c14e3f208328a0752134
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2004-08-09 10:00:06 +0000

    add classpath
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@1780 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- art.bat
+++ art.bat
@@ -1,2 +1,9 @@
-@java uk.ac.sanger.artemis.components.ArtemisMain
+@echo off
+
+REM
+REM Batch file for starting Artemis on windows
+REM
+
+REM execute Artemis
+start javaw -classpath .;lib\biojava.jar;lib\jobcontrol.jar;lib\jemAlign.jar uk.ac.sanger.artemis.components.ArtemisMain
 
