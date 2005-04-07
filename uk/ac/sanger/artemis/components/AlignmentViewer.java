e60beb35d75364fcdce132cf4938b5dc813b4257
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2005-04-07 14:15:36 +0000

    fix
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@2385 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/components/AlignmentViewer.java
+++ uk/ac/sanger/artemis/components/AlignmentViewer.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/AlignmentViewer.java,v 1.18 2005-04-07 13:57:21 tjc Exp $
+ * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/AlignmentViewer.java,v 1.19 2005-04-07 14:15:36 tjc Exp $
  */
 
 package uk.ac.sanger.artemis.components;
@@ -47,7 +47,7 @@ import javax.swing.*;
  *  ComparisonData object.
  *
  *  @author Kim Rutherford
- *  @version $Id: AlignmentViewer.java,v 1.18 2005-04-07 13:57:21 tjc Exp $
+ *  @version $Id: AlignmentViewer.java,v 1.19 2005-04-07 14:15:36 tjc Exp $
  **/
 
 public class AlignmentViewer extends CanvasPanel
@@ -1597,7 +1597,7 @@ public class AlignmentViewer extends CanvasPanel
             next_start = jstart;
         }
 
-        if(next_start > start)
+        if(next_start > start+1)
         {
           coords[0] = new Integer(start+1);
           coords[1] = new Integer(next_start-1);
