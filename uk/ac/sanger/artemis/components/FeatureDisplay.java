29da22da7b84103192ac1cdb9f90197656aca8c3
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2005-06-17 12:24:35 +0000

    dont need to repaint twice in scrollbar adjustment
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@2907 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/components/FeatureDisplay.java
+++ uk/ac/sanger/artemis/components/FeatureDisplay.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/FeatureDisplay.java,v 1.24 2005-05-06 10:20:26 tjc Exp $
+ * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/FeatureDisplay.java,v 1.25 2005-06-17 12:24:35 tjc Exp $
  */
 
 package uk.ac.sanger.artemis.components;
@@ -45,7 +45,7 @@ import javax.swing.JComponent;
  *  This component is used for displaying an Entry.
  *
  *  @author Kim Rutherford
- *  @version $Id: FeatureDisplay.java,v 1.24 2005-05-06 10:20:26 tjc Exp $
+ *  @version $Id: FeatureDisplay.java,v 1.25 2005-06-17 12:24:35 tjc Exp $
  **/
 
 public class FeatureDisplay extends EntryGroupPanel
@@ -4087,7 +4087,7 @@ public class FeatureDisplay extends EntryGroupPanel
 
           fireAdjustmentEvent(DisplayAdjustmentEvent.SCROLL_ADJUST_EVENT);
           needVisibleFeatureVectorUpdate();
-          repaint();
+//        repaint();
         }
       }
     });
