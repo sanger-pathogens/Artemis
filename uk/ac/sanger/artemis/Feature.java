6b5f34ea93419f5839ee0ecf38b8df7feb1446e3
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2005-07-15 10:15:06 +0000

    show exons on frame line
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@3180 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/Feature.java
+++ uk/ac/sanger/artemis/Feature.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/Feature.java,v 1.10 2005-06-07 14:55:13 tjc Exp $
+ * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/Feature.java,v 1.11 2005-07-15 10:15:06 tjc Exp $
  */
 
 package uk.ac.sanger.artemis;
@@ -59,7 +59,7 @@ import java.util.Date;
  *  embl.Feature and embl.Entry objects.
  *
  *  @author Kim Rutherford
- *  @version $Id: Feature.java,v 1.10 2005-06-07 14:55:13 tjc Exp $
+ *  @version $Id: Feature.java,v 1.11 2005-07-15 10:15:06 tjc Exp $
  **/
 
 public class Feature
@@ -571,6 +571,7 @@ public class Feature
   public boolean isProteinFeature() 
   {
     if(getKey().toString().startsWith("CDS") ||
+       getKey().equals("exon") ||
        getKey().equals("BLASTCDS") || 
        getKey().equals("polypeptide"))
       return true;
