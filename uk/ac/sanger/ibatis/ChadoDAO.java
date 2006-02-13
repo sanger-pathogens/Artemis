db8c27669727a47d135e8d0d9379d86bc74ac4dc
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-02-13 15:37:40 +0000

    tidy and comment
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@4084 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/ibatis/ChadoDAO.java
+++ uk/ac/sanger/ibatis/ChadoDAO.java
@@ -31,28 +31,79 @@ import java.util.List;
 public interface ChadoDAO
 {
 
+  /**
+   *
+   * Get the residues of a feature.
+   * @param feature_id  id of feature to query
+   * @param schema      schema/organism name or null
+   *
+   */
   public Feature getSequence(final int feature_id,
                              final String schema)
                         throws SQLException;
 
+  /**
+   *
+   * Get feature name given the feature_id and schema.
+   * @param feature_id  id of feature to query
+   * @param schema      schema/organism name or null
+   *
+   */
   public String getFeatureName(final int feature_id,
                                final String schema)
                        throws SQLException;
 
+  /**
+   *
+   * Get child feature properties for a given parent
+   * feature to be able to construct a GFF like feature.
+   *
+   * @param parentFeatureID  id of parent feature to query
+   * @param schema           schema/organism name or null
+   *
+   */
   public List getGff(final int parentFeatureID,
                      final String schema)
                      throws SQLException;
 
+  /**
+   *
+   * Given a list of distict cvterm_id/type_id's of feature types
+   * that have residues in the given schema and the schema name
+   * return a list of features in the schema with residues.
+   * @param cvterm_ids list of cvterm_id/type_id's
+   * @param schema      schema/organism name or null
+   *
+   */
   public List getResidueFeatures(List cvterm_ids,
                                  final String schema)
                      throws SQLException;
 
+  /**
+   *
+   * For a schema return the type_id's with residues.
+   * @param schema      schema/organism name or null
+   * @return list of type_id's
+   * 
+   */
   public List getResidueType(final String schema)
                      throws SQLException;
 
+  /**
+   * 
+   * Get available schemas (as a List of Feature objects).
+   *
+   */
   public List getSchema()
               throws SQLException;
 
+  /**
+   * 
+   * Get a lst of the cvterm's.
+   * @param name cvterm name
+   * @param cv_name ontology name (e.g. gene, sequence)
+   *
+   */
   public List getCvterm()
               throws SQLException;
 }
