bf85fa3fc8301decc73389cc528c15811f475630
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-03-22 17:13:58 +0000

    tidy
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@4185 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/chado/ChadoFeature.java
+++ uk/ac/sanger/artemis/chado/ChadoFeature.java
@@ -29,6 +29,12 @@ import java.util.Date;
 import java.util.Hashtable;
 import java.util.Vector;
 
+/**
+*
+* Chado feature representation of feature, featureloc, featureprop,
+* feature_relationship tables
+*
+**/
 public class ChadoFeature
 {
  
@@ -69,81 +75,161 @@ public class ChadoFeature
   /** phase */
   private int phase;
 
+  /**
+   *
+   * Get the feature_id.
+   *
+   */
   public int getId()
   {
     return id;
   } 
 
+  /**
+   *
+   * Set the feature_id.
+   *
+   */
   public void setId(int id)
   {
     this.id = id;
   }
 
+  /**
+   *
+   * Get the postgres schema.
+   *
+   */
   public String getSchema()
   {
     return schema;
   }
 
+  /**
+   *
+   * Set the postgres schema.
+   *
+   */
   public void setSchema(String schema)
   {
     this.schema = schema;
   }
 
+  /**
+   *
+   * Get the parent feature_id.
+   *
+   */
   public String getObject_id()
   {
     return object_id;
   }
 
+  /**
+   *
+   * Set the parent feature_id.
+   *
+   */
   public void setObject_id(String object_id)
   {
     this.object_id = object_id;
   }
 
+  /**
+   *
+   * Get the last time feature was modified.
+   *
+   */
   public Date getTimelastmodified()
   {
     return timelastmodified;
   }
 
+  /**
+   *
+   * Set the last time feature was modified.
+   *
+   */
   public void setTimelastmodified(Date timelastmodified)
   {
     this.timelastmodified = timelastmodified;
   }
 
+  /**
+   *
+   * Get the strand.
+   *
+   */
   public int getStrand()
   {
     return strand;
   }
 
+  /**
+   *
+   * Set the strand.
+   *
+   */
   public void setStrand(int strand)
   {
     this.strand = strand;
   }
 
+  /**
+   *
+   * Get the fmin value.
+   *
+   */
   public int getFmin()
   {
     return fmin;
   }
 
+  /**
+   *
+   * Set the fmin value.
+   *
+   */
   public void setFmin(int fmin)
   {
     this.fmin = fmin;
   }
 
+  /**
+   *
+   * Get the fmax value.
+   *
+   */
   public int getFmax()
   {
     return fmax;
   }
 
+  /**
+   *
+   * Set the fmax value.
+   *
+   */
   public void setFmax(int fmax)
   {
     this.fmax = fmax;
   }
 
+  /**
+   *
+   * Get sequence length.
+   *
+   */
   public int getLength()
   {
     return length;
   }
 
+  /**
+   *
+   * Set sequence length.
+   *
+   */
   public void setLength(int length)
   {
     this.length = length;
