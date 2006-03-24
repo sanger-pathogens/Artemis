e8bab5e5a590a22a7452330c5c75e92b1f167977
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-03-24 15:07:38 +0000

    deleteFeature()
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@4201 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/chado/IBatisDAO.java
+++ uk/ac/sanger/artemis/chado/IBatisDAO.java
@@ -331,7 +331,6 @@ public class IBatisDAO implements ChadoDAO
                      throws SQLException
   {
     SqlMapClient sqlMap = DbSqlConfig.getSqlMapInstance();
-    tsn.setSchema(schema);
     ChadoFeature chadoFeature = new ChadoFeature();
     chadoFeature.setSchema(schema);
     chadoFeature.setUniquename(tsn.getUniqueName());
