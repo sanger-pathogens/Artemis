d1d1e4f5dc2798cb2bf52941d4bb6de222758356
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2006-07-14 13:42:55 +0000

    more on transcript drawing
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@4541 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/components/genebuilder/GeneViewerPanel.java
+++ uk/ac/sanger/artemis/components/genebuilder/GeneViewerPanel.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/genebuilder/GeneViewerPanel.java,v 1.6 2006-07-07 15:20:20 tjc Exp $
+ * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/genebuilder/GeneViewerPanel.java,v 1.7 2006-07-14 13:42:55 tjc Exp $
  */
 
 package uk.ac.sanger.artemis.components.genebuilder;
@@ -90,26 +90,17 @@ public class GeneViewerPanel extends JPanel
       (uk.ac.sanger.artemis.Feature)embl_gene.getUserData();
     
     setFont(uk.ac.sanger.artemis.Options.getOptions().getFont());
-    //final FontMetrics fm = this.getFontMetrics(getFont());
 
     int start = embl_gene.getFirstBase();
     int end   = embl_gene.getLastBase();
-    
-    BasicStroke stroke = new BasicStroke(4.f);
-    Stroke original_stroke = g2d.getStroke();
-    g2d.setStroke(stroke);
     g2d.setColor( gene.getColour() );
-    
     int ypos = border;
     
     // draw gene
     g2d.drawString(gene.getIDString(), border, ypos);
-    drawFeature(g2d, border, getSize().width - border, ypos, gene.getColour());
+    drawFeature(g2d, border, getSize().width - border, ypos, gene.getColour(), 1);
     
-    List transcripts = chado_gene.getTranscripts();
-    Feature embl_transcript;
-    uk.ac.sanger.artemis.Feature transcript;
-    
+    List transcripts = chado_gene.getTranscripts();   
     float fraction = (float)(getSize().width - (2*border))/
                      (float)(end-start);
     
@@ -117,157 +108,296 @@ public class GeneViewerPanel extends JPanel
     
     for(int i=0; i<transcripts.size(); i++)
     {
+      /*
       // draw frame lines
+   
       if(!overlay_transcripts || i == 0)
-        drawFrameLines(g2d, ypos, stroke,
+        drawFrameLines(g2d, ypos, 
             start, end, fraction);
-
-      embl_transcript  = (Feature)transcripts.get(i);
-      transcript = (uk.ac.sanger.artemis.Feature)embl_transcript.getUserData();
+    
+      drawTranscriptOnFrameLine(g2d, (Feature)transcripts.get(i), 
+          start, end, ypos, 
+          fraction);
+   
+      if(!overlay_transcripts)
+        ypos += 9 * getFontHeight() * 2;
+      */
       
-      int t_start = border+(int)((embl_transcript.getFirstBase()-start)*fraction);
-      int t_end   = border+(int)((embl_transcript.getLastBase()-start)*fraction);
-      
-      g2d.setColor( transcript.getColour() );
-      int nframe;
-      if(!embl_transcript.getLocation().isComplement())
-        nframe = (3 * getFontHeight() * 2) ;
-      else
-        nframe = (4 * getFontHeight() * 2) ;
-      
-      g2d.drawString(transcript.getIDString(), border, ypos+nframe);
-      drawFeature(g2d, t_start, t_end, ypos+nframe, transcript.getColour());
-      
-      try
-      {          
-        List exons = chado_gene.getExonsOfTranscript(
-            (String)embl_transcript.getQualifierByName("ID").getValues().get(0));
-        
-        if(exons == null)
-        {
-          if(!overlay_transcripts)
-            ypos += 9 * getFontHeight() * 2;
-          continue;
-        }
-        
-        int offset = 0;
-        
-        if(exons.get(0) instanceof ChadoFeature)
-        {
-          int last_ex_start = 0;
-          int last_ex_end   = 0;
-          int last_ypos     = 0;
-          
-          ChadoFeature start_exon = (ChadoFeature)exons.get(0);
-          ChadoFeatureLoc loc = ChadoFeature.getFeatureLoc(
-               start_exon.getFeaturelocsForFeatureId(), chado_gene.getSrcfeature_id());
-          
-          if(loc.getStrand() == -1)
-          {
-            ChadoFeatureLoc loc_last = ChadoFeature.getFeatureLoc(
-                ((ChadoFeature)exons.get(exons.size()-1)).getFeaturelocsForFeatureId(),
-                chado_gene.getSrcfeature_id());
-                
-            if(loc.getFmin() < loc_last.getFmin())
-              Collections.reverse(exons);
-          }
-          
-          for(int j=0; j<exons.size(); j++)
-          {           
-            ChadoFeature exon = (ChadoFeature)exons.get(j);
-            loc = ChadoFeature.getFeatureLoc(
-                exon.getFeaturelocsForFeatureId(), chado_gene.getSrcfeature_id());
-            
-            int ex_start = border+(int)((loc.getFmin()+1-start)*fraction);
-            int ex_end   = border+(int)((loc.getFmax()-start)*fraction);
-               
-            Color exon_col = getColorFromAttributes(exon);
-         
-            offset = getFrameID(chado_gene, loc, j, exons) * getFontHeight() * 2;
-            
-            boolean isForward = false;
-            if(loc.getStrand() == 1)
-              isForward = true;
-            
-            drawExons(g2d, ex_start, ex_end, 
-                      last_ex_start, last_ex_end, last_ypos,
-                      offset, ypos, exon_col,
-                      original_stroke, stroke, isForward);
-            
-            last_ex_end   = ex_end;
-            last_ex_start = ex_start;
-            last_ypos   = ypos+offset;
-          }
-          
-          if(!overlay_transcripts)
-            ypos += 9 * getFontHeight() * 2;
-          continue;
-        }
-        
-        for(int j=0; j<exons.size(); j++)
-        {
-          int last_ex_start = 0;
-          int last_ex_end   = 0;
-          int last_ypos     = 0;
-          
-          Feature embl_exon = (Feature)exons.get(j);
-          
-          uk.ac.sanger.artemis.Feature exon = 
-            (uk.ac.sanger.artemis.Feature)embl_exon.getUserData();
-            
-          FeatureSegmentVector segments = exon.getSegments();
-
-          for(int k=0; k<segments.size(); k++)
-          {
-            FeatureSegment segment = segments.elementAt(k);
-            
-            Range range = segment.getRawRange();
-            offset = segment.getFrameID() * getFontHeight() * 2;
-            
-            int ex_start = border+(int)((range.getStart()-start)*fraction);
-            int ex_end   = border+(int)((range.getEnd()-start)*fraction);
-
-            if(exon.getColour() != null)
-              g2d.setColor( exon.getColour() );
-                       
-            drawExons(g2d, ex_start, ex_end, 
-                     last_ex_start, last_ex_end, last_ypos,
-                     offset, ypos, exon.getColour(),
-                     original_stroke, stroke, segment.isForwardSegment());
-            
-            last_ex_end   = ex_end;
-            last_ex_start = ex_start;
-            last_ypos   = ypos+offset;
-            
-            
-          }
-          
-          //ypos += (8 * getFontHeight() * 2) + (getFontHeight()/2) + border;
-          if(!overlay_transcripts)
-            ypos += 9 * getFontHeight() * 2;
-        }
-        
-      }
-      catch(InvalidRelationException e)
-      {
-        e.printStackTrace();
-      }
+      ypos += (2 * border) + (getFontHeight() * 4);
+      drawTranscriptOnLine(g2d, (Feature)transcripts.get(i), 
+                           start, end, ypos, 
+                           fraction);
     }
     setPreferredSize(new Dimension(getSize().width, ypos+border));
   }
   
   
+  private void drawTranscriptOnFrameLine(Graphics2D g2d, Feature embl_transcript, 
+                                         int start, int end, int ypos, 
+                                         float fraction)
+  {
+
+    uk.ac.sanger.artemis.Feature transcript = 
+       (uk.ac.sanger.artemis.Feature)embl_transcript.getUserData();
+    
+    int t_start = border+(int)((embl_transcript.getFirstBase()-start)*fraction);
+    int t_end   = border+(int)((embl_transcript.getLastBase()-start)*fraction);
+    
+    g2d.setColor( transcript.getColour() );
+    int nframe;
+    if(!embl_transcript.getLocation().isComplement())
+      nframe = (3 * getFontHeight() * 2) ;
+    else
+      nframe = (4 * getFontHeight() * 2) ;
+    
+    g2d.drawString(transcript.getIDString(), border, ypos+nframe);
+    drawFeature(g2d, t_start, t_end, ypos+nframe, transcript.getColour(), 1);
+    
+    try
+    {          
+      List exons = chado_gene.getExonsOfTranscript(
+          (String)embl_transcript.getQualifierByName("ID").getValues().get(0));
+      
+      if(exons == null)
+      {
+        if(!overlay_transcripts)
+          ypos += 9 * getFontHeight() * 2;
+        return;
+      }
+      
+      int offset = 0;
+      
+      if(exons.get(0) instanceof ChadoFeature)
+      {
+        int last_ex_start = 0;
+        int last_ex_end   = 0;
+        int last_ypos     = 0;
+        
+        ChadoFeature start_exon = (ChadoFeature)exons.get(0);
+        ChadoFeatureLoc loc = ChadoFeature.getFeatureLoc(
+             start_exon.getFeaturelocsForFeatureId(), chado_gene.getSrcfeature_id());
+        
+        if(loc.getStrand() == -1)
+        {
+          ChadoFeatureLoc loc_last = ChadoFeature.getFeatureLoc(
+              ((ChadoFeature)exons.get(exons.size()-1)).getFeaturelocsForFeatureId(),
+              chado_gene.getSrcfeature_id());
+              
+          if(loc.getFmin() < loc_last.getFmin())
+            Collections.reverse(exons);
+        }
+        
+        for(int j=0; j<exons.size(); j++)
+        {           
+          ChadoFeature exon = (ChadoFeature)exons.get(j);
+          loc = ChadoFeature.getFeatureLoc(
+              exon.getFeaturelocsForFeatureId(), chado_gene.getSrcfeature_id());
+          
+          int ex_start = border+(int)((loc.getFmin()+1-start)*fraction);
+          int ex_end   = border+(int)((loc.getFmax()-start)*fraction);
+             
+          Color exon_col = getColorFromAttributes(exon);
+       
+          offset = getFrameID(chado_gene, loc, j, exons) * getFontHeight() * 2;
+          
+          boolean isForward = false;
+          if(loc.getStrand() == 1)
+            isForward = true;
+          
+          drawExons(g2d, ex_start, ex_end, 
+                    last_ex_start, last_ex_end, last_ypos,
+                    offset, ypos, exon_col,
+                    1, isForward);
+          
+          last_ex_end   = ex_end;
+          last_ex_start = ex_start;
+          last_ypos   = ypos+offset;
+        }
+
+        return;
+      }
+      
+      
+      // build from artemis objects
+      for(int j=0; j<exons.size(); j++)
+      {
+        int last_ex_start = 0;
+        int last_ex_end   = 0;
+        int last_ypos     = 0;
+        
+        Feature embl_exon = (Feature)exons.get(j);
+        
+        uk.ac.sanger.artemis.Feature exon = 
+          (uk.ac.sanger.artemis.Feature)embl_exon.getUserData();
+          
+        FeatureSegmentVector segments = exon.getSegments();
+
+        for(int k=0; k<segments.size(); k++)
+        {
+          FeatureSegment segment = segments.elementAt(k);
+          
+          Range range = segment.getRawRange();
+          offset = segment.getFrameID() * getFontHeight() * 2;
+          
+          int ex_start = border+(int)((range.getStart()-start)*fraction);
+          int ex_end   = border+(int)((range.getEnd()-start)*fraction);
+
+          if(exon.getColour() != null)
+            g2d.setColor( exon.getColour() );
+                     
+          drawExons(g2d, ex_start, ex_end, 
+                   last_ex_start, last_ex_end, last_ypos,
+                   offset, ypos, exon.getColour(),
+                   1, segment.isForwardSegment());
+          
+          last_ex_end   = ex_end;
+          last_ex_start = ex_start;
+          last_ypos   = ypos+offset;
+          
+          
+        }
+        
+
+      }
+      
+    }
+    catch(InvalidRelationException e)
+    {
+      e.printStackTrace();
+    }
+  
+  }
+  
+  
+  private void drawTranscriptOnLine(Graphics2D g2d, Feature embl_transcript, 
+      int start, int end, int ypos, 
+      float fraction)
+  {
+    BasicStroke stroke = new BasicStroke(48.f);
+    g2d.setStroke(stroke);
+    
+    uk.ac.sanger.artemis.Feature transcript = 
+       (uk.ac.sanger.artemis.Feature)embl_transcript.getUserData();
+
+    int t_start = border+(int)((embl_transcript.getFirstBase()-start)*fraction);
+    int t_end   = border+(int)((embl_transcript.getLastBase()-start)*fraction);
+
+    g2d.setColor( transcript.getColour() );
+
+    g2d.drawString(transcript.getIDString(), border, ypos);
+    drawFeature(g2d, t_start, t_end, ypos, transcript.getColour(), 1);
+
+    try
+    {          
+      List exons = chado_gene.getExonsOfTranscript(
+       (String)embl_transcript.getQualifierByName("ID").getValues().get(0));
+
+      if(exons == null)
+        return; 
+
+      ypos += border*2;
+      
+      if(exons.get(0) instanceof ChadoFeature)
+      {
+        int last_ex_start = 0;
+        int last_ex_end   = 0;
+        int last_ypos     = 0;
+
+        ChadoFeature start_exon = (ChadoFeature)exons.get(0);
+        ChadoFeatureLoc loc = ChadoFeature.getFeatureLoc(
+            start_exon.getFeaturelocsForFeatureId(), chado_gene.getSrcfeature_id());
+
+        if(loc.getStrand() == -1)
+        {
+          ChadoFeatureLoc loc_last = ChadoFeature.getFeatureLoc(
+              ((ChadoFeature)exons.get(exons.size()-1)).getFeaturelocsForFeatureId(),
+              chado_gene.getSrcfeature_id());
+
+          if(loc.getFmin() < loc_last.getFmin())
+            Collections.reverse(exons);
+        }
+
+
+        for(int j=0; j<exons.size(); j++)
+        {
+          ChadoFeature exon = (ChadoFeature)exons.get(j);
+          loc = ChadoFeature.getFeatureLoc(
+              exon.getFeaturelocsForFeatureId(), chado_gene.getSrcfeature_id());
+
+          int ex_start = border+(int)((loc.getFmin()+1-start)*fraction);
+          int ex_end   = border+(int)((loc.getFmax()-start)*fraction);
+
+          Color exon_col = getColorFromAttributes(exon);
+
+          boolean isForward = false;
+          if(loc.getStrand() == 1)
+            isForward = true;
+
+          drawExons(g2d, ex_start, ex_end, 
+              last_ex_start, last_ex_end, last_ypos,
+              0, ypos, exon_col,
+              2, isForward);
+
+          last_ex_end   = ex_end;
+          last_ex_start = ex_start;
+          last_ypos   = ypos;
+        }
+
+        return;
+      }
+
+      // build from artemis objects
+      for(int j=0; j<exons.size(); j++)
+      {
+        int last_ex_start = 0;
+        int last_ex_end   = 0;
+        int last_ypos     = 0;
+
+        Feature embl_exon = (Feature)exons.get(j);
+
+        uk.ac.sanger.artemis.Feature exon = 
+          (uk.ac.sanger.artemis.Feature)embl_exon.getUserData();
+
+        FeatureSegmentVector segments = exon.getSegments();
+
+        for(int k=0; k<segments.size(); k++)
+        {
+          FeatureSegment segment = segments.elementAt(k);
+
+          Range range = segment.getRawRange();
+
+          int ex_start = border+(int)((range.getStart()-start)*fraction);
+          int ex_end   = border+(int)((range.getEnd()-start)*fraction);
+
+          if(exon.getColour() != null)
+            g2d.setColor( exon.getColour() );
+
+          drawExons(g2d, ex_start, ex_end, 
+              last_ex_start, last_ex_end, last_ypos,
+              0, ypos, exon.getColour(),
+              2, segment.isForwardSegment());
+
+          last_ex_end   = ex_end;
+          last_ex_start = ex_start;
+          last_ypos   = ypos;
+        }
+      }
+    }
+    catch(InvalidRelationException e)
+    {
+      e.printStackTrace();
+    }
+  }
+  
   /**
    * Draw frame lines
    * @param g2d
    * @param ypos
-   * @param stroke
    * @param start
    * @param end
    * @param fraction
    */
   private void drawFrameLines(Graphics2D g2d, int ypos,
-                              Stroke stroke,
                               int start, int end, float fraction)
   {
     int offset;
@@ -282,21 +412,23 @@ public class GeneViewerPanel extends JPanel
       g2d.drawLine(border, ypos+offset, 
                    (int)((end-start)*fraction)+border, ypos+offset);
     }
-    g2d.setStroke(stroke);
   }
   
   private void drawExons(Graphics2D g2d, int ex_start, int ex_end, 
                          int last_ex_start, int last_ex_end, int last_ypos,
                          int offset, int ypos, Color exon_colour,
-                         Stroke original_stroke, Stroke stroke, boolean isForward)
+                         int size,
+                         boolean isForward)
   {   
-    drawFeature(g2d, ex_start, ex_end, ypos+offset, exon_colour);
+    drawFeature(g2d, ex_start, ex_end, ypos+offset, exon_colour, size);
     
     // draw connections
     if(last_ex_end != 0 ||
        last_ex_start != 0)
     {
-      g2d.setStroke(original_stroke);
+      BasicStroke stroke = new BasicStroke(1.f);
+      g2d.setStroke(stroke);
+      
       int ymid;
       if(last_ypos < ypos+offset)
         ymid = last_ypos;
@@ -316,25 +448,33 @@ public class GeneViewerPanel extends JPanel
                      last_ex_start+((ex_end-last_ex_start)/2), ymid-getFontHeight()/2);
         g2d.drawLine(last_ex_start+((ex_end-last_ex_start)/2), ymid-getFontHeight()/2, 
                      ex_end, ypos+offset); 
-      }
-      g2d.setStroke(stroke);  
+      }  
     }
 
   }
   
+  /**
+   * Draw rectangular box for a feature.
+   * @param g2d
+   * @param start   start of feature
+   * @param end     end of feature
+   * @param ypos    y position
+   * @param colour  feature colour
+   * @param size    parameter to control the height of the feature
+   */
   private void drawFeature(Graphics2D g2d, int start, int end, 
-                           int ypos, Color colour)
+                           int ypos, Color colour, int size)
   {
     RoundRectangle2D e = new RoundRectangle2D.Float(start, ypos, 
         end-start,
-        getFontHeight(), 0, ypos);
+        getFontHeight()*size, 0, ypos);
 
     if(colour == null)
       colour = Color.BLACK;
     
     GradientPaint gp = new GradientPaint(start, ypos, 
         colour,
-        start, ypos+(getFontHeight()/2), 
+        start, ypos+( (getFontHeight()/2) * size ), 
         Color.white, true);
     g2d.setPaint(gp); 
     g2d.fill(e);
