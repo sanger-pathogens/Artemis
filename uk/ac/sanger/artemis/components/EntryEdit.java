f9af2460ae2ccf12ea8fd075eb3c2b11c3f0cccd
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-03-02 15:47:43 +0000

    remove ChadoTransactionManager listeners when closing the EntryEdit
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@5577 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/components/EntryEdit.java
+++ uk/ac/sanger/artemis/components/EntryEdit.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/EntryEdit.java,v 1.35 2007-02-21 10:55:49 tjc Exp $
+ * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/EntryEdit.java,v 1.36 2007-03-02 15:47:43 tjc Exp $
  */
 
 package uk.ac.sanger.artemis.components;
@@ -54,7 +54,7 @@ import java.util.List;
  *  Each object of this class is used to edit an EntryGroup object.
  *
  *  @author Kim Rutherford
- *  @version $Id: EntryEdit.java,v 1.35 2007-02-21 10:55:49 tjc Exp $
+ *  @version $Id: EntryEdit.java,v 1.36 2007-03-02 15:47:43 tjc Exp $
  *
  */
 public class EntryEdit extends JFrame
@@ -575,6 +575,11 @@ public class EntryEdit extends JFrame
   private void entryEditFinished() 
   {
     setVisible(false);
+   
+    // chado transaction manager
+    getEntryGroup().removeFeatureChangeListener(ctm);
+    getEntryGroup().removeEntryChangeListener(ctm);
+    
     getEntryGroup().removeFeatureChangeListener(selection);
     getEntryGroup().removeEntryChangeListener(selection);
 
