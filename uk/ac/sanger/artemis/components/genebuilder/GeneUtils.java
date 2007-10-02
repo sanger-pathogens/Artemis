6323bba551d451ba31b14cf5949358d32454f5fd
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-10-02 14:10:19 +0000

    add createGeneModel
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@6481 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/components/genebuilder/GeneUtils.java
+++ uk/ac/sanger/artemis/components/genebuilder/GeneUtils.java
@@ -28,18 +28,37 @@ import java.util.Vector;
 import javax.swing.Box;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
+import uk.ac.sanger.artemis.components.EditMenu;
+import uk.ac.sanger.artemis.components.MessageDialog;
+import uk.ac.sanger.artemis.components.SelectionMenu;
+import uk.ac.sanger.artemis.io.ChadoCanonicalGene;
 import uk.ac.sanger.artemis.io.DatabaseDocumentEntry;
+import uk.ac.sanger.artemis.io.EntryInformationException;
 import uk.ac.sanger.artemis.io.Feature;
 import uk.ac.sanger.artemis.io.GFFStreamFeature;
+import uk.ac.sanger.artemis.io.Key;
+import uk.ac.sanger.artemis.io.Location;
+import uk.ac.sanger.artemis.io.Qualifier;
+import uk.ac.sanger.artemis.io.QualifierVector;
+import uk.ac.sanger.artemis.sequence.MarkerRange;
+import uk.ac.sanger.artemis.util.DatabaseDocument;
+import uk.ac.sanger.artemis.util.OutOfRangeException;
+import uk.ac.sanger.artemis.util.ReadOnlyException;
+import uk.ac.sanger.artemis.Entry;
 import uk.ac.sanger.artemis.EntryGroup;
 import uk.ac.sanger.artemis.EntryVector;
+import uk.ac.sanger.artemis.FeatureKeyQualifierPredicate;
+import uk.ac.sanger.artemis.FeaturePredicate;
 import uk.ac.sanger.artemis.FeatureVector;
+import uk.ac.sanger.artemis.GotoEventSource;
+import uk.ac.sanger.artemis.Selection;
 
 
 public class GeneUtils
@@ -145,6 +164,11 @@ public class GeneUtils
     showHideGeneFeatures(features);
   }
   
+  /**
+   * Based on the hidenFeatures and showFeatures set the GFFStreamFeatures
+   * visibility
+   * @param features
+   */
   public static void showHideGeneFeatures(final FeatureVector features)
   {
     for(int i=0; i<features.size(); i++)
@@ -162,9 +186,160 @@ public class GeneUtils
     }
   }
   
+  /**
+   * Determine based on the feature given if a feature is hidden
+   * @param key
+   * @return
+   */
   public static boolean isHiddenFeature(final String key)
   {
     return hideFeatures.contains(key);
+  }
+  
+  /**
+   * 
+   * @param frame
+   * @param selection
+   * @param entry_group
+   * @param goto_event_source
+   */
+  public static void createGeneModel(final JFrame frame,
+      final Selection selection,
+      final EntryGroup entry_group,
+      final GotoEventSource goto_event_source)
+  {
+    if(!SelectionMenu.checkForSelectionRange(frame, selection))
+      return;
+    final MarkerRange range = selection.getMarkerRange ();
+    final Entry default_entry = entry_group.getDefaultEntry ();
+
+    if (default_entry == null) 
+    {
+      new MessageDialog (frame, "There is no default entry");
+      return;
+    }
+    
+    final QualifierVector qualifiers = new QualifierVector();
+    final String uniquename = promptForUniquename(entry_group, 
+                                   range.isForwardMarker());
+    final Qualifier qualifier = new Qualifier("ID", uniquename);
+    qualifiers.add(qualifier);
+    
+    try
+    {
+      final Location new_location = range.createLocation ();
+      final Key key = new Key("gene");
+      final uk.ac.sanger.artemis.Feature geneFeature = 
+          default_entry.createFeature(key, new_location, qualifiers);
+
+      final ChadoCanonicalGene chadoGene = new ChadoCanonicalGene();
+      chadoGene.setGene(geneFeature.getEmblFeature());
+      ((uk.ac.sanger.artemis.io.GFFStreamFeature) 
+          (geneFeature.getEmblFeature())).setChadoGene(chadoGene);
+      
+      // create transcript
+      uk.ac.sanger.artemis.Feature transcript = 
+        GeneViewerPanel.createTranscript(chadoGene, entry_group);
+      ((uk.ac.sanger.artemis.io.GFFStreamFeature)
+          (transcript.getEmblFeature())).setChadoGene(chadoGene);
+      final String transcriptId = 
+         (String)transcript.getQualifierByName("ID").getValues().get(0);
+      
+      // add exon
+      GeneViewerPanel.addExonFeature(chadoGene, entry_group, 
+          null, range.getRange(), transcriptId, selection, 
+          new Key(DatabaseDocument.EXONMODEL), null);
+      
+      // add protein
+      uk.ac.sanger.artemis.Feature polypep =
+        GeneViewerPanel.addProteinFeature(chadoGene, entry_group, transcriptId, transcript);
+      
+      selection.clear();
+      selection.add(polypep);
+      
+      EditMenu.editSelectedFeatures(entry_group, selection, 
+          goto_event_source, polypep, null, null);
+    }
+    catch(ReadOnlyException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    catch(EntryInformationException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    catch(OutOfRangeException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+  
+  /**
+   * Prompt the user for an ID
+   * @return
+   */
+  public static String promptForUniquename(final EntryGroup entry_group,
+                                            final boolean is_forward)
+  {
+    final Entry default_entry = entry_group.getDefaultEntry ();
+    String id = null;
+    
+    if(default_entry.getEMBLEntry() instanceof 
+        uk.ac.sanger.artemis.io.DatabaseDocumentEntry)
+    {  
+      while(id == null ||
+            id.equals("") ||
+            id.equals("to_be_set"))
+      {
+        String msg = "Provide a unique ID ";
+        
+        if(!is_forward)
+          msg = msg + "for reverse strand : ";
+        else
+          msg = msg + ": ";
+        
+        id = JOptionPane.showInputDialog(null,
+                           msg,
+                           "ID missing ",
+                           JOptionPane.QUESTION_MESSAGE).trim();
+        
+        if(!isUniqueID(entry_group, id))
+        {
+          JOptionPane.showMessageDialog(null, 
+              "ID "+id+" not unique.\nEnter a unique ID.", 
+              "ID Not Unique", 
+              JOptionPane.WARNING_MESSAGE);
+          id = null;
+        }
+      }
+    }
+    return id;
+  }
+  
+  /**
+   * Test to ensure ID (chado uniquename) is unique.
+   * @param entry_group
+   * @param id
+   * @return
+   */
+  private static boolean isUniqueID(final EntryGroup entry_group,
+                                    final String id)
+  {
+    final FeaturePredicate predicate =
+      new FeatureKeyQualifierPredicate(null, "ID", id, 
+                                       false, true);
+    final FeatureVector features = entry_group.getAllFeatures();
+    for(int i=0; i<features.size(); i++)
+    {
+      uk.ac.sanger.artemis.Feature feature = features.elementAt(i);
+      if(predicate.testPredicate(feature))
+        return false;
+      
+    }
+    return true;
   }
   
   /**
