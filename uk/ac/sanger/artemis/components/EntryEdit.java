31ab24a71d17650ef0e45b6df822a3d819ac5c9e
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-10-11 16:21:07 +0000

    commitToDatabase made static
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@6560 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/components/EntryEdit.java
+++ uk/ac/sanger/artemis/components/EntryEdit.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/EntryEdit.java,v 1.52 2007-10-11 16:12:26 tjc Exp $
+ * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/EntryEdit.java,v 1.53 2007-10-11 16:21:07 tjc Exp $
  */
 
 package uk.ac.sanger.artemis.components;
@@ -67,7 +67,7 @@ import java.util.Vector;
  *  Each object of this class is used to edit an EntryGroup object.
  *
  *  @author Kim Rutherford
- *  @version $Id: EntryEdit.java,v 1.52 2007-10-11 16:12:26 tjc Exp $
+ *  @version $Id: EntryEdit.java,v 1.53 2007-10-11 16:21:07 tjc Exp $
  *
  */
 public class EntryEdit extends JFrame
@@ -1523,7 +1523,7 @@ public class EntryEdit extends JFrame
 
   }
   
-  protected boolean commitToDatabase(final EntryGroup entry_group,
+  protected static boolean commitToDatabase(final EntryGroup entry_group,
                                          final ChadoTransactionManager ctm,
                                          final JFrame frame,
                                          final Selection selection,
