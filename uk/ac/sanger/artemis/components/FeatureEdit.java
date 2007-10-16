efaba324e0ffd2b81640b1300caa5fd0933cba88
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2007-10-16 16:03:29 +0000

    change to MatchPanel constructor
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@6580 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/components/FeatureEdit.java
+++ uk/ac/sanger/artemis/components/FeatureEdit.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/FeatureEdit.java,v 1.49 2007-10-09 16:09:18 tjc Exp $
+ * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/FeatureEdit.java,v 1.50 2007-10-16 16:03:29 tjc Exp $
  **/
 
 package uk.ac.sanger.artemis.components;
@@ -68,7 +68,7 @@ import javax.swing.*;
  *  FeatureEdit class
  *
  *  @author Kim Rutherford
- *  @version $Id: FeatureEdit.java,v 1.49 2007-10-09 16:09:18 tjc Exp $
+ *  @version $Id: FeatureEdit.java,v 1.50 2007-10-16 16:03:29 tjc Exp $
  **/
 public class FeatureEdit extends JPanel
                          implements EntryChangeListener, FeatureChangeListener 
@@ -897,7 +897,8 @@ public class FeatureEdit extends JPanel
       cvForm = new CVPanel(getFeature());
       cvForm.setBackground(Color.WHITE);
 
-      matchForm = new MatchPanel(getFeature());
+      matchForm = new MatchPanel(getFeature(), 
+          (DocumentEntry)getFeature().getEmblFeature().getEntry());
       matchForm.setBackground(Color.WHITE);
       
       gffPanel = new GffPanel(getFeature());
