54f9c5916749a42748827353fbc3f4325b01e66d
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-02-08 14:37:03 +0000

    fix
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@7143 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/components/FileViewer.java
+++ uk/ac/sanger/artemis/components/FileViewer.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/FileViewer.java,v 1.17 2008-02-08 14:34:34 tjc Exp $
+ * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/FileViewer.java,v 1.18 2008-02-08 14:37:03 tjc Exp $
  */
 
 package uk.ac.sanger.artemis.components;
@@ -60,7 +60,7 @@ import uk.ac.sanger.artemis.Options;
  *  be viewed.
  *
  *  @author Kim Rutherford
- *  @version $Id: FileViewer.java,v 1.17 2008-02-08 14:34:34 tjc Exp $
+ *  @version $Id: FileViewer.java,v 1.18 2008-02-08 14:37:03 tjc Exp $
  *
  **/
 
@@ -144,7 +144,7 @@ public class FileViewer extends JFrame
     textPane.setEditable(false);
     textPane.setFont(font);
     textPane.setBackground(Color.white);
-    textPane.setPreferredSize(d);
+    textPane.setMinimumSize(d);
     
     getContentPane().add(scroller, "Center");
 
