995dd2e4ad2fe8f4bafab3a281eb6787c5d02c01
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-05-15 09:57:31 +0000

    add option to take logs
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@7433 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/plot/UserDataAlgorithm.java
+++ uk/ac/sanger/artemis/plot/UserDataAlgorithm.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/plot/UserDataAlgorithm.java,v 1.3 2005-10-11 14:20:31 tjc Exp $
+ * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/plot/UserDataAlgorithm.java,v 1.4 2008-05-15 09:57:31 tjc Exp $
  */
 
 package uk.ac.sanger.artemis.plot;
@@ -39,180 +39,11 @@ import java.io.*;
  *  set in the constructor.
  *
  *  @author Kim Rutherford <kmr@sanger.ac.uk>
- *  @version $Id: UserDataAlgorithm.java,v 1.3 2005-10-11 14:20:31 tjc Exp $
+ *  @version $Id: UserDataAlgorithm.java,v 1.4 2008-05-15 09:57:31 tjc Exp $
  **/
 
-public class UserDataAlgorithm extends BaseAlgorithm {
-  /**
-   *  Create a new UserDataAlgorithm object.
-   *  @param strand The strand to do the calculation on.
-   *  @param document The Document to read the data from.
-   **/
-  public UserDataAlgorithm (final Strand strand, final Document document)
-      throws IOException {
-    super (strand, "User algorithm from " + document.getName (), "user");
-
-    final Reader document_reader = document.getReader ();
-
-    LinePushBackReader pushback_reader =
-      new LinePushBackReader (document_reader);
-
-    final String first_line = pushback_reader.readLine ();
-
-    final StringVector tokens = StringVector.getStrings (first_line, " ");
-
-    if (tokens.size () < 1) {
-      throw new ReadFormatException ("unknown file type");
-    }
-
-    this.number_of_values = tokens.size ();
-
-    pushback_reader.pushBack (first_line);
-
-    data = new float [strand.getSequenceLength ()][tokens.size ()];
-
-    readData (pushback_reader);
-  }
-
-  /**
-   *  Read all from buffered_reader into data.
-   **/
-  private void readData (final LinePushBackReader pushback_reader)
-      throws IOException {
-    String line = null;
-
-    int count = 0;
-
-    while ((line = pushback_reader.readLine ()) != null) {
-
-      if (count >= getStrand ().getSequenceLength ()) {
-        throw new ReadFormatException ("too many values in input file");
-      }
-
-      final StringVector tokens = StringVector.getStrings (line, " ");
-
-      final int read_base;
-
-      if (tokens.size () == data[0].length) {
-        for (int i = 0 ; i < tokens.size () ; ++i) {
-          try {
-            final float value =
-              Float.valueOf ((String)tokens.elementAt (i)).floatValue ();
-
-            if (value > data_max) {
-              data_max = value;
-            }
-
-            if (value < data_min) {
-              data_min = value;
-            }
-
-            data[count][i] = value;
-
-            average_value += value;
-          } catch (NumberFormatException e) {
-            throw new ReadFormatException ("cannot understand this number: " +
-                                           tokens.elementAt (i) + " - " +
-                                           e.getMessage ());
-          }
-        }
-      } else {
-        throw new ReadFormatException ("line has the wrong number of fields");
-      }
-
-      ++count;
-    }
-
-    average_value /= data[0].length * getStrand ().getSequenceLength ();
-  }
-
-  /**
-   *  Return the value of the function between a pair of bases.
-   *  @param start The start base (included in the range).
-   *  @param end The end base (included in the range).
-   *  @param values The one return value for this algorithm is returned in
-   *    this array.
-   **/
-  public void getValues (int start, int end, final float [] values) {
-    final int value_count = getValueCount ();
-
-    for (int i = 0 ; i < value_count ; ++i) {
-      values [i] = 0;
-      for (int base = start ; base <= end ; ++base) {
-        values [i] += data[base - 1][i] / (end - start + 1);
-      }
-    }
-  }
-
-  /**
-   *  Return the number of values a call to getValues () will return - one
-   *  in this case.
-   **/
-  public int getValueCount () {
-    return number_of_values;
-  }
-
-  /**
-   *  Return the default or optimal window size.
-   *  @return null is returned if this algorithm doesn't have optimal window
-   *    size.
-   **/
-  public Integer getDefaultWindowSize () {
-    return new Integer (3);
-  }
-
-  /**
-   *  Return the default maximum window size for this algorithm.
-   *  @return null is returned if this algorithm doesn't have maximum window
-   *    size.
-   **/
-  public Integer getDefaultMaxWindowSize () {
-    return new Integer (100);
-  }
-
-  /**
-   *  Return the default minimum window size for this algorithm.
-   *  @return null is returned if this algorithm doesn't have minimum window
-   *    size.
-   **/
-  public Integer getDefaultMinWindowSize () {
-    return new Integer (1);
-  }
-
-  /**
-   *  Return the default or optimal step size.
-   *  @return null is returned if this algorithm doesn't have optimal step
-   *    size.
-   **/
-  public Integer getDefaultStepSize (int window_size) {
-    if (window_size > 10) {
-      return new Integer (window_size / 10);
-    } else {
-      return null;
-    }
-  }
-
-  /**
-   *  Return the maximum value of this algorithm.
-   **/
-  protected Float getMaximumInternal () {
-    return new Float (data_max);
-  }
-
-  /**
-   *  Return the minimum value of this algorithm.
-   **/
-  protected Float getMinimumInternal () {
-    return new Float (data_min);
-  }
-
-  /**
-   *  Return the average value of function over the whole strand.
-   **/
-  public Float getAverage () {
-    return new Float (average_value);
-  }
-
+public class UserDataAlgorithm extends BaseAlgorithm
+{
   /**
    *  The data that was read by the constructor.
    **/
@@ -237,4 +68,188 @@ public class UserDataAlgorithm extends BaseAlgorithm {
    *  The value returned by getValueCount ().
    **/
   private int number_of_values;
+  
+  private boolean logTransform;
+  
+  /**
+   *  Create a new UserDataAlgorithm object.
+   *  @param strand The strand to do the calculation on.
+   *  @param document The Document to read the data from.
+   **/
+  public UserDataAlgorithm (final Strand strand, final Document document, 
+                            final boolean logTransform)
+      throws IOException 
+  {
+    super (strand, "User algorithm from " + document.getName (), "user");
+
+    this.logTransform = logTransform;
+    final Reader document_reader = document.getReader ();
+
+    LinePushBackReader pushback_reader = new LinePushBackReader (document_reader);
+
+    final String first_line = pushback_reader.readLine ();
+    final StringVector tokens = StringVector.getStrings (first_line, " ");
+
+    if (tokens.size () < 1) 
+      throw new ReadFormatException ("unknown file type");
+
+    this.number_of_values = tokens.size ();
+    pushback_reader.pushBack (first_line);
+    data = new float [strand.getSequenceLength ()][tokens.size ()];
+
+    readData (pushback_reader);
+  }
+
+  /**
+   *  Read all from buffered_reader into data.
+   **/
+  private void readData (final LinePushBackReader pushback_reader)
+      throws IOException
+  {
+    String line = null;
+    int count = 0;
+
+    while ((line = pushback_reader.readLine ()) != null)
+    {
+
+      if (count >= getStrand ().getSequenceLength ()) 
+        throw new ReadFormatException ("too many values in input file");
+
+      final StringVector tokens = StringVector.getStrings (line, " ");
+      //final int read_base;
+
+      if (tokens.size () == data[0].length) 
+      {
+        for (int i = 0 ; i < tokens.size () ; ++i)
+        {
+          try 
+          {
+            float value =
+              Float.valueOf ((String)tokens.elementAt (i)).floatValue ();
+
+            if(logTransform)
+              value = (float) Math.log(value+1);
+            
+            if (value > data_max) 
+              data_max = value;
+
+            if (value < data_min)
+              data_min = value;
+
+            data[count][i] = value;
+
+            average_value += value;
+          } 
+          catch (NumberFormatException e) 
+          {
+            throw new ReadFormatException ("cannot understand this number: " +
+                                           tokens.elementAt (i) + " - " +
+                                           e.getMessage ());
+          }
+        }
+      } 
+      else 
+        throw new ReadFormatException ("line has the wrong number of fields");
+
+      ++count;
+    }
+
+    average_value /= data[0].length * getStrand ().getSequenceLength ();
+  }
+
+  /**
+   *  Return the value of the function between a pair of bases.
+   *  @param start The start base (included in the range).
+   *  @param end The end base (included in the range).
+   *  @param values The one return value for this algorithm is returned in
+   *    this array.
+   **/
+  public void getValues (int start, int end, final float [] values) 
+  {
+    final int value_count = getValueCount ();
+
+    for (int i = 0 ; i < value_count ; ++i) 
+    {
+      values [i] = 0;
+      for (int base = start ; base <= end ; ++base) 
+        values [i] += data[base - 1][i] / (end - start + 1);
+    }
+  }
+
+  /**
+   *  Return the number of values a call to getValues () will return - one
+   *  in this case.
+   **/
+  public int getValueCount () 
+  {
+    return number_of_values;
+  }
+
+  /**
+   *  Return the default or optimal window size.
+   *  @return null is returned if this algorithm doesn't have optimal window
+   *    size.
+   **/
+  public Integer getDefaultWindowSize () 
+  {
+    return new Integer (3);
+  }
+
+  /**
+   *  Return the default maximum window size for this algorithm.
+   *  @return null is returned if this algorithm doesn't have maximum window
+   *    size.
+   **/
+  public Integer getDefaultMaxWindowSize ()
+  {
+    return new Integer (100);
+  }
+
+  /**
+   *  Return the default minimum window size for this algorithm.
+   *  @return null is returned if this algorithm doesn't have minimum window
+   *    size.
+   **/
+  public Integer getDefaultMinWindowSize () 
+  {
+    return new Integer (1);
+  }
+
+  /**
+   *  Return the default or optimal step size.
+   *  @return null is returned if this algorithm doesn't have optimal step
+   *    size.
+   **/
+  public Integer getDefaultStepSize (int window_size)
+  {
+    if (window_size > 10) 
+      return new Integer (window_size / 10);
+    else 
+      return null;
+  }
+
+  /**
+   *  Return the maximum value of this algorithm.
+   **/
+  protected Float getMaximumInternal ()
+  {
+    return new Float (data_max);
+  }
+
+  /**
+   *  Return the minimum value of this algorithm.
+   **/
+  protected Float getMinimumInternal () 
+  {
+    return new Float (data_min);
+  }
+
+  /**
+   *  Return the average value of function over the whole strand.
+   **/
+  public Float getAverage () 
+  {
+    return new Float (average_value);
+  }
+
 }
