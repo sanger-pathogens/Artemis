0999de820da09d1c628d40102204ebee9459d431
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2008-10-15 13:56:42 +0000

    tidy
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@9030 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/components/MessageDialog.java
+++ uk/ac/sanger/artemis/components/MessageDialog.java
@@ -20,33 +20,56 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/MessageDialog.java,v 1.1 2004-06-09 09:47:07 tjc Exp $
+ * $Header: //tmp/pathsoft/artemis/uk/ac/sanger/artemis/components/MessageDialog.java,v 1.2 2008-10-15 13:56:42 tjc Exp $
  **/
 
 package uk.ac.sanger.artemis.components;
 
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import uk.ac.sanger.artemis.Options;
 
-import java.awt.*;
-import java.awt.event.*;
-
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 /**
  *  A popup dialog box that displays a message and has an OK JButton.
  *
  *  @author Kim Rutherford
- *  @version $Id: MessageDialog.java,v 1.1 2004-06-09 09:47:07 tjc Exp $
+ *  @version $Id: MessageDialog.java,v 1.2 2008-10-15 13:56:42 tjc Exp $
  **/
 
-public class MessageDialog extends JDialog {
+public class MessageDialog extends JDialog 
+{
+  private static final long serialVersionUID = 1L;
+
+  /** Messages longer than this will be put in a TextArea rather than a Label. */
+  final private int MESSAGE_SPLIT_SIZE = 100;
+
+  final private JButton ok_button = new JButton ("OK");
+  
   /**
    *  Create a blocking MessageDialog component.
    *  @param parent The parent window.
    *  @param message The message to display in the JDialog and to use as the
    *    frame title.
    **/
-  public MessageDialog (final JFrame parent, final String message) {
+  public MessageDialog (final JFrame parent, final String message)
+  {
     this (parent, message, message, true);
   }
   
@@ -59,7 +82,8 @@ public class MessageDialog extends JDialog {
    *    shown.
    **/
   public MessageDialog (final JFrame parent, final String message,
-                        final boolean modal) {
+                        final boolean modal) 
+  {
     this (parent, message, message, modal);
   }
   
@@ -71,7 +95,8 @@ public class MessageDialog extends JDialog {
    **/
   public MessageDialog (final JFrame parent_frame,
                         final String title,
-                        final String message) {
+                        final String message) 
+  {
     this (parent_frame, title, message, true);
   }
 
@@ -86,15 +111,17 @@ public class MessageDialog extends JDialog {
   public MessageDialog (final JFrame parent_frame,
                         final String title,
                         final String message,
-                        final boolean modal) {
+                        final boolean modal) 
+  {
     super (parent_frame, title, modal);
 
     final Font font = Options.getOptions ().getFont ();
     setFont (font);
 
-    if (message.length () < MESSAGE_SPLIT_SIZE) {
+    if (message.length () < MESSAGE_SPLIT_SIZE) 
       getContentPane ().add (new JLabel (message), "North"); 
-    } else {
+    else 
+    {
       final JTextArea text_area = new JTextArea (18, 90);
       text_area.setText (message);
 
@@ -103,41 +130,38 @@ public class MessageDialog extends JDialog {
     }
 
     final JPanel panel = new JPanel ();
-
     panel.add (ok_button);
-    ok_button.addActionListener (new ActionListener () {
-      public void actionPerformed (ActionEvent e) {
+    ok_button.addActionListener (new ActionListener () 
+    {
+      public void actionPerformed (ActionEvent e) 
+      {
         MessageDialog.this.dispose ();
       }
     });
 
-    addWindowListener (new WindowAdapter () {
-      public void windowClosing (WindowEvent event) {
+    addWindowListener (new WindowAdapter () 
+    {
+      public void windowClosing (WindowEvent event) 
+      {
         MessageDialog.this.dispose ();
       }
     });
 
-    addKeyListener (new KeyAdapter () {
-      public void keyTyped(final KeyEvent e) {
+    addKeyListener (new KeyAdapter () 
+    {
+      public void keyTyped(final KeyEvent e) 
+      {
         MessageDialog.this.dispose ();
       }
     });
     
     getContentPane ().add (panel, "South");
     pack ();
-    
+
     final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
     setLocation (new Point ((screen.width - getSize ().width) / 2,
                             (screen.height - getSize ().height) / 2));
-
     setVisible (true);
   }
-
-  /**
-   *  Messages longer than this will be put in a TextArea rather than a Label.
-   **/
-  final private int MESSAGE_SPLIT_SIZE = 100;
-
-  final private JButton ok_button = new JButton ("OK");
 }
 
