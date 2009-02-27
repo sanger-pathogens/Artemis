cd464e410238a0ec3fdc7ce63d80b7470ccbd06b
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-02-27 12:30:26 +0000

    new constructor
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@9939 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/circular/digest/InSilicoGelPanel.java
+++ uk/ac/sanger/artemis/circular/digest/InSilicoGelPanel.java
@@ -44,19 +44,18 @@ import javax.swing.JRadioButtonMenuItem;
 
 import uk.ac.sanger.artemis.components.FileViewer;
 
-public class InSilicoGelPanel extends JPanel
-                              implements ActionListener
+public class InSilicoGelPanel extends JPanel implements ActionListener
 {
-	private static final long serialVersionUID = 1L;
-	private int marginHeight = 50;
-	private int marginWidth  = 60;
-	private int panelHeight;
-	private List<FragmentBand> genomeFragments = new Vector<FragmentBand>();
-	private int MAX_FRAGMENT_LENGTH = 0;
-	private int MIN_FRAGMENT_LENGTH = Integer.MAX_VALUE;
-	private boolean drawLog = false;
-	private JPopupMenu popup;
-	private File restrictOutput;
+  private static final long serialVersionUID = 1L;
+  private int marginHeight = 50;
+  private int marginWidth = 60;
+  private int panelHeight;
+  private List<FragmentBand> genomeFragments = new Vector<FragmentBand>();
+  private int MAX_FRAGMENT_LENGTH = 0;
+  private int MIN_FRAGMENT_LENGTH = Integer.MAX_VALUE;
+  private boolean drawLog = false;
+  private JPopupMenu popup;
+  private File restrictOutput;
 
   /**
    * @param genomeLength
@@ -64,235 +63,262 @@ public class InSilicoGelPanel extends JPanel
    * @param panelHeight
    * @param restrictOutput
    */
-	public InSilicoGelPanel(final int genomeLength,
-			                    final List<CutSite> cutSites,
-			                    final int panelHeight,
-			                    final File restrictOutput)
-	{
-		this.panelHeight    = panelHeight;
-		this.restrictOutput = restrictOutput;
+  public InSilicoGelPanel(final int genomeLength, final List<CutSite> cutSites,
+      final int panelHeight, final File restrictOutput)
+  {
+    this.panelHeight = panelHeight;
+    this.restrictOutput = restrictOutput;
 
-		setBackground(Color.white);
-		setPreferredSize(new Dimension(160,panelHeight));
-		
-		Integer len;
-		int firstSiteEnd = 0;
-		int lastSite = 0;
-		for(int i=0; i<cutSites.size(); i++)
-		{
-			CutSite cutSite = cutSites.get(i);
-			if(i == 0)
-				firstSiteEnd = cutSite.getFivePrime()+1;
-			else
-			{
-				len = cutSite.getFivePrime()-lastSite;
-				
-				FragmentBand band = new FragmentBand();
-				band.genomeFragmentLength = len;
-				band.bandCutSite = cutSite;
-				genomeFragments.add(band);
-				if(len > MAX_FRAGMENT_LENGTH)
-					MAX_FRAGMENT_LENGTH = len;
-				if(len < MIN_FRAGMENT_LENGTH)
-					MIN_FRAGMENT_LENGTH = len;
-			}
-			lastSite = cutSite.getFivePrime();
-		}
+    Integer len;
+    int firstSiteEnd = 0;
+    int lastSite = 0;
+    for (int i = 0; i < cutSites.size(); i++)
+    {
+      CutSite cutSite = cutSites.get(i);
+      if (i == 0)
+        firstSiteEnd = cutSite.getFivePrime() + 1;
+      else
+      {
+        len = cutSite.getFivePrime() - lastSite;
 
-		len = genomeLength-lastSite+firstSiteEnd;
-		FragmentBand band = new FragmentBand();
-		band.genomeFragmentLength = len;
-		band.bandCutSite = cutSites.get(0);
-		genomeFragments.add(band);
-		if(len > MAX_FRAGMENT_LENGTH)
-			MAX_FRAGMENT_LENGTH = len;
-		//System.out.println(len.toString());
-		
-		MouseListener popupListener = new PopupListener();
-		addMouseListener(popupListener);
-		
-		popup = new JPopupMenu();
-		JRadioButtonMenuItem  linearScale = new JRadioButtonMenuItem ("Linear scale");
+        FragmentBand band = new FragmentBand();
+        band.genomeFragmentLength = len;
+        band.bandCutSite = cutSite;
+        genomeFragments.add(band);
+        if (len > MAX_FRAGMENT_LENGTH)
+          MAX_FRAGMENT_LENGTH = len;
+        if (len < MIN_FRAGMENT_LENGTH)
+          MIN_FRAGMENT_LENGTH = len;
+      }
+      lastSite = cutSite.getFivePrime();
+    }
+
+    len = genomeLength - lastSite + firstSiteEnd;
+    FragmentBand band = new FragmentBand();
+    band.genomeFragmentLength = len;
+    band.bandCutSite = cutSites.get(0);
+    genomeFragments.add(band);
+    if (len > MAX_FRAGMENT_LENGTH)
+      MAX_FRAGMENT_LENGTH = len;
+    // System.out.println(len.toString());
+
+    init();
+  }
+  
+  public InSilicoGelPanel(final List<FragmentBand> genomeFragments,
+      final int panelHeight, final File restrictOutput)
+  {
+    this.panelHeight = panelHeight;
+    this.restrictOutput = restrictOutput;
+    this.genomeFragments = genomeFragments;
+   
+    for(int i=0; i<genomeFragments.size(); i++)
+    {
+      int len = genomeFragments.get(i).genomeFragmentLength;
+      if (len > MAX_FRAGMENT_LENGTH)
+        MAX_FRAGMENT_LENGTH = len;
+      if (len < MIN_FRAGMENT_LENGTH)
+        MIN_FRAGMENT_LENGTH = len;
+    }
+    init();
+  }
+  
+  /**
+   * Initialise the panel
+   */
+  private void init()
+  {
+    setBackground(Color.white);
+    setPreferredSize(new Dimension(160, panelHeight));
+    
+    MouseListener popupListener = new PopupListener();
+    addMouseListener(popupListener);
+
+    popup = new JPopupMenu();
+    JRadioButtonMenuItem linearScale = new JRadioButtonMenuItem("Linear scale");
     popup.add(linearScale);
     linearScale.addActionListener(this);
-    JRadioButtonMenuItem  logScale = new JRadioButtonMenuItem ("Log scale");
+    JRadioButtonMenuItem logScale = new JRadioButtonMenuItem("Log scale");
     popup.add(logScale);
     logScale.addActionListener(this);
     ButtonGroup group = new ButtonGroup();
     group.add(linearScale);
     group.add(logScale);
     linearScale.setSelected(true);
-    
+
     JMenuItem showCutSites = new JMenuItem("Show cut site details");
     popup.add(showCutSites);
     showCutSites.addActionListener(this);
-	}
-	
-	/**
-	 * Override 
-	 */
-	public void paintComponent(Graphics g)
-	{
-		super.paintComponent(g);
-		
-		Graphics2D g2D = (Graphics2D)g;
-		g2D.draw3DRect(marginWidth, (marginHeight/2), marginWidth, panelHeight-(marginHeight), true);
-		
-		int gelHeight = (panelHeight-(2*marginHeight));
-		g2D.setColor(Color.blue);
-	
-		BasicStroke stroke  = new BasicStroke(1.f);
-		BasicStroke stroke2 = new BasicStroke(2.f);
-		g2D.setStroke(stroke);
-		
-		for(int i=0; i<genomeFragments.size(); i++)
-		{
-			int fragmentLength = genomeFragments.get(i).genomeFragmentLength;
+  }
 
-			final int y;
-			if(isDrawLog())
-			  y = getLogValue(fragmentLength, marginHeight, gelHeight);
-			else
-				y = gelHeight+marginHeight-(int)( ((float)(gelHeight)/
-						(float)(MAX_FRAGMENT_LENGTH-MIN_FRAGMENT_LENGTH)) * fragmentLength );
-			
-			if(!genomeFragments.get(i).bandCutSite.isHighlighted())
-			{
-				g2D.setStroke(stroke);
-				g2D.setColor(Color.blue);
-			}
-			else
-			{
-				g2D.setColor(Color.black);
-				g2D.drawString(genomeFragments.get(i).bandCutSite.getEnzymeName(),
-	                     marginWidth+marginWidth+2, y);
-				g2D.setStroke(stroke2);
-				g2D.setColor(Color.yellow);
-			}
-			g2D.drawLine(marginWidth, y, marginWidth+marginWidth, y);
-		}
-		
-		drawScale(g2D, stroke,gelHeight);
-	}
-	
-	/**
-	 * Draw the fragment length scale
-	 * @param g2D
-	 * @param stroke
-	 * @param gelHeight
-	 */
-	private void drawScale(Graphics2D g2D, BasicStroke stroke, int gelHeight)
-	{
-		g2D.setColor(Color.black);
-		g2D.setStroke(stroke);
-		NumberFormat formatter = new DecimalFormat("#0.0");
-		
-		int nscale = 8;
-		if(isDrawLog())
-			nscale = 5;
-		
-  	float range = (MAX_FRAGMENT_LENGTH-MIN_FRAGMENT_LENGTH)/(float)nscale;
-	  for(int i=0;i<nscale+1; i++)
-		{
-			float length = MIN_FRAGMENT_LENGTH+(range*i);
-				
-			int y;
-			if(isDrawLog())
-				y = getLogValue((int)length, marginHeight, gelHeight);
-			else
-				y = gelHeight+marginHeight-(int)( ((float)(gelHeight)/
-		  		(float)(MAX_FRAGMENT_LENGTH-MIN_FRAGMENT_LENGTH)) * length );
-			
-			 g2D.drawLine(marginWidth, y, marginWidth-10, y);
-			 
-			 g2D.drawString(formatter.format(length/1000)+"kb",0,y); 
-		}
-	}
-	
-  private static final double LOG10SCALE = 1.d/Math.log(10);
-  
+  /**
+   * Override
+   */
+  public void paintComponent(Graphics g)
+  {
+    super.paintComponent(g);
+
+    Graphics2D g2D = (Graphics2D) g;
+    g2D.draw3DRect(marginWidth, (marginHeight / 2), marginWidth, panelHeight
+        - (marginHeight), true);
+
+    int gelHeight = (panelHeight - (2 * marginHeight));
+    g2D.setColor(Color.blue);
+
+    BasicStroke stroke = new BasicStroke(1.f);
+    BasicStroke stroke2 = new BasicStroke(2.f);
+    g2D.setStroke(stroke);
+
+    for (int i = 0; i < genomeFragments.size(); i++)
+    {
+      int fragmentLength = genomeFragments.get(i).genomeFragmentLength;
+
+      final int y;
+      if (isDrawLog())
+        y = getLogValue(fragmentLength, marginHeight, gelHeight);
+      else
+        y = gelHeight
+            + marginHeight
+            - (int) (((float) (gelHeight) / (float) (MAX_FRAGMENT_LENGTH - MIN_FRAGMENT_LENGTH)) * fragmentLength);
+
+      if ((genomeFragments.get(i).bandCutSite == null) || 
+          !genomeFragments.get(i).bandCutSite.isHighlighted())
+      {
+        g2D.setStroke(stroke);
+        g2D.setColor(Color.blue);
+      }
+      else
+      {
+        g2D.setColor(Color.black);
+        g2D.drawString(genomeFragments.get(i).bandCutSite.getEnzymeName(),
+            marginWidth + marginWidth + 2, y);
+        g2D.setStroke(stroke2);
+        g2D.setColor(Color.yellow);
+      }
+      g2D.drawLine(marginWidth, y, marginWidth + marginWidth, y);
+    }
+
+    drawScale(g2D, stroke, gelHeight);
+  }
+
+  /**
+   * Draw the fragment length scale
+   * 
+   * @param g2D
+   * @param stroke
+   * @param gelHeight
+   */
+  private void drawScale(Graphics2D g2D, BasicStroke stroke, int gelHeight)
+  {
+    g2D.setColor(Color.black);
+    g2D.setStroke(stroke);
+    NumberFormat formatter = new DecimalFormat("#0.0");
+
+    int nscale = 8;
+    if (isDrawLog())
+      nscale = 5;
+
+    float range = (MAX_FRAGMENT_LENGTH - MIN_FRAGMENT_LENGTH) / (float) nscale;
+    for (int i = 0; i < nscale + 1; i++)
+    {
+      float length = MIN_FRAGMENT_LENGTH + (range * i);
+
+      int y;
+      if (isDrawLog())
+        y = getLogValue((int) length, marginHeight, gelHeight);
+      else
+        y = gelHeight
+            + marginHeight
+            - (int) (((float) (gelHeight) / (float) (MAX_FRAGMENT_LENGTH - MIN_FRAGMENT_LENGTH)) * length);
+
+      g2D.drawLine(marginWidth, y, marginWidth - 10, y);
+
+      g2D.drawString(formatter.format(length / 1000) + "kb", 0, y);
+    }
+  }
+
+  private static final double LOG10SCALE = 1.d / Math.log(10);
+
   /**
    * Get base 10 commons log
+   * 
    * @param val
    * @return
    */
-  private static double log10(double val) { return Math.log(val) * LOG10SCALE; }
-	
-  private int getLogValue(int val, int gelStart, int gelRange) 
+  private static double log10(double val)
   {
-      double log_low  = log10(MIN_FRAGMENT_LENGTH);
-      double log_high = log10(MAX_FRAGMENT_LENGTH);
-      double log_val  = log10(val);
-
-      double log_unit = ((double)gelRange) / (log_high - log_low);
-      return (int)((double)(gelRange + gelStart) - ((log_val - log_low) * log_unit));
+    return Math.log(val) * LOG10SCALE;
   }
- 
+
+  private int getLogValue(int val, int gelStart, int gelRange)
+  {
+    double log_low = log10(MIN_FRAGMENT_LENGTH);
+    double log_high = log10(MAX_FRAGMENT_LENGTH);
+    double log_val = log10(val);
+
+    double log_unit = ((double) gelRange) / (log_high - log_low);
+    return (int) ((double) (gelRange + gelStart) - ((log_val - log_low) * log_unit));
+  }
+
   private boolean isDrawLog()
-	{
-		return drawLog;
-	}
+  {
+    return drawLog;
+  }
 
   private void setDrawLog(boolean drawLog)
-	{
-		this.drawLog = drawLog;
-	}
-  
-  class FragmentBand
   {
-  	CutSite bandCutSite;
-  	int genomeFragmentLength;
+    this.drawLog = drawLog;
   }
-  
-	class PopupListener extends MouseAdapter
-	{
-		public void mousePressed(MouseEvent e)
-		{
-			maybeShowPopup(e);
-		}
 
-		public void mouseReleased(MouseEvent e)
-		{
-			maybeShowPopup(e);
-		}
+  class PopupListener extends MouseAdapter
+  {
+    public void mousePressed(MouseEvent e)
+    {
+      maybeShowPopup(e);
+    }
 
-		private void maybeShowPopup(MouseEvent e)
-		{
-			if (e.isPopupTrigger())
-				popup.show(e.getComponent(), e.getX(), e.getY());
-		}
-	}
+    public void mouseReleased(MouseEvent e)
+    {
+      maybeShowPopup(e);
+    }
 
-	public void actionPerformed(ActionEvent e)
-	{
-		if (e.getSource() instanceof JRadioButtonMenuItem)
-		{
-			JRadioButtonMenuItem radioButton = (JRadioButtonMenuItem) e.getSource();
-			if (radioButton.isSelected())
-			{
-				setDrawLog(radioButton.getText().startsWith("Log"));
-				repaint();
-			}
-		}
-		else
-		{
-			final FileViewer viewer = new FileViewer(
-					restrictOutput.getName(), true, false);
-			BufferedReader br;
-			try
-			{
-				br = new BufferedReader(new FileReader(restrictOutput));
+    private void maybeShowPopup(MouseEvent e)
+    {
+      if (e.isPopupTrigger())
+        popup.show(e.getComponent(), e.getX(), e.getY());
+    }
+  }
 
-				StringBuffer buff = new StringBuffer();
-				String line;
-				while ((line = br.readLine()) != null)
-					buff.append(line + "\n");
-				viewer.getTextPane().setText(buff.toString());
-			} 
-			catch (Exception e2)
-			{
-				e2.printStackTrace();
-			}
-		}
-	}
+  public void actionPerformed(ActionEvent e)
+  {
+    if (e.getSource() instanceof JRadioButtonMenuItem)
+    {
+      JRadioButtonMenuItem radioButton = (JRadioButtonMenuItem) e.getSource();
+      if (radioButton.isSelected())
+      {
+        setDrawLog(radioButton.getText().startsWith("Log"));
+        repaint();
+      }
+    }
+    else
+    {
+      final FileViewer viewer = new FileViewer(restrictOutput.getName(), true,
+          false);
+      BufferedReader br;
+      try
+      {
+        br = new BufferedReader(new FileReader(restrictOutput));
+
+        StringBuffer buff = new StringBuffer();
+        String line;
+        while ((line = br.readLine()) != null)
+          buff.append(line + "\n");
+        viewer.getTextPane().setText(buff.toString());
+      }
+      catch (Exception e2)
+      {
+        e2.printStackTrace();
+      }
+    }
+  }
 }
