f614b7a7e68b255dc60430b203269747bd256c0f
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-08-20 13:55:34 +0000

    replaced stars with spaces in password entry
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@11613 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/io/UIEraserThread.java
+++ uk/ac/sanger/artemis/io/UIEraserThread.java
@@ -16,7 +16,7 @@ class UIEraserThread implements Runnable {
    public void run () {
       stop = true;
       while (stop) {
-         System.out.print("\010*");
+         System.out.print("\010 ");
 	     try {
 	         Thread.currentThread().sleep(1);
          } catch(InterruptedException ie) {
