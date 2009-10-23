9a2fde3d5d39378254c936d9ebcfb77bad972ffa
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-10-23 14:54:05 +0000

    add generics
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@12182 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/io/ChadoCanonicalGene.java
+++ uk/ac/sanger/artemis/io/ChadoCanonicalGene.java
@@ -53,20 +53,24 @@ public class ChadoCanonicalGene
   private Feature gene;
   
   // part_of gene
-  private List transcripts = new Vector();
+  private List<Feature> transcripts = new Vector<Feature>();
   
   // part_of transcripts
-  private Hashtable splicedFeatures = new Hashtable();
+  private Hashtable<String, List<Feature>> splicedFeatures =
+    new Hashtable<String, List<Feature>>();
   
   // derives_from transript
-  private Hashtable proteins = new Hashtable();
+  private Hashtable<String, Feature> proteins = new Hashtable<String, Feature>();
 
   // utr features
-  private Hashtable three_prime_utr = new Hashtable();
-  private Hashtable five_prime_utr  = new Hashtable();
+  private Hashtable<String,  List<Feature>> three_prime_utr =
+    new Hashtable<String,  List<Feature>>();
+  private Hashtable<String,  List<Feature>> five_prime_utr  = 
+    new Hashtable<String,  List<Feature>>();
   
   // other child features of transcript
-  private Hashtable other_features = new Hashtable();
+  private Hashtable<String,  List<Feature>> other_features = 
+    new Hashtable<String,  List<Feature>>();
   
   // srcfeature
   private int srcfeature_id;
@@ -152,7 +156,7 @@ public class ChadoCanonicalGene
    */
   public void updateUniqueName(final String oldName, 
                                final String newName,
-                               final Set children)
+                               final Set<Feature> children)
   {
     updateNames(splicedFeatures,oldName,newName);
     updateNames(proteins,oldName,newName);
@@ -200,7 +204,7 @@ public class ChadoCanonicalGene
         return;
       }
       
-      final Enumeration enum_protein = proteins.keys();
+      final Enumeration<String> enum_protein = proteins.keys();
       while(enum_protein.hasMoreElements())
       {
         final String transcriptName = (String)enum_protein.nextElement();
@@ -216,7 +220,7 @@ public class ChadoCanonicalGene
       if(feature != null)
       {
         String transcript_name = getQualifier((Feature) feature, "Parent");
-        List utr = get3UtrOfTranscript(transcript_name);
+        List<Feature> utr = get3UtrOfTranscript(transcript_name);
         utr.remove(feature);
         return;
       }
@@ -225,7 +229,7 @@ public class ChadoCanonicalGene
       if(feature != null)
       {
         String transcript_name = getQualifier((Feature) feature, "Parent");
-        List utr = get5UtrOfTranscript(transcript_name);
+        List<Feature> utr = get5UtrOfTranscript(transcript_name);
         utr.remove(feature);
         return;
       }
@@ -234,7 +238,7 @@ public class ChadoCanonicalGene
       if(feature != null)
       {
         String transcript_name = getQualifier((Feature) feature, "Parent");
-        List others = getOtherFeaturesOfTranscript(transcript_name);
+        List<Feature> others = getOtherFeaturesOfTranscript(transcript_name);
         others.remove(feature);
         return;
       }
@@ -252,9 +256,9 @@ public class ChadoCanonicalGene
    * @param embl_feature
    * @return
    */
-  public Set getChildren(Feature embl_feature)
+  public Set<Feature> getChildren(Feature embl_feature)
   {
-    Set children = new HashSet();
+    Set<Feature> children = new HashSet<Feature>();
     try
     {
       String name = getQualifier(embl_feature, "ID");
@@ -262,10 +266,10 @@ public class ChadoCanonicalGene
       String gene_name = getQualifier(getGene(), "ID");
       if(name.equals(gene_name))
       {
-        List transcripts = getTranscripts();
+        List<Feature> transcripts = getTranscripts();
         for(int i=0; i<transcripts.size(); i++)
         {
-          Feature transcript = (Feature)transcripts.get(i);
+          Feature transcript = transcripts.get(i);
           children.add(transcript);
           children.addAll( getChildren(transcript) );
         }
@@ -278,10 +282,10 @@ public class ChadoCanonicalGene
       searchForChildren(other_features, name, children);
       
       // protein
-      Enumeration pep_enum = proteins.elements();
+      Enumeration<Feature> pep_enum = proteins.elements();
       while(pep_enum.hasMoreElements())
       {
-        Feature child = (Feature)pep_enum.nextElement();
+        Feature child = pep_enum.nextElement();
         String parent = getQualifier(child, "Derives_from");
         if(parent != null && parent.equals(name))
           children.add(child);
@@ -304,20 +308,21 @@ public class ChadoCanonicalGene
    * @param children    collection to add child features to
    * @throws InvalidRelationException
    */
-  private void searchForChildren(Hashtable hash, String parent_id,
-                                 Set children)
+  private void searchForChildren(Hashtable<String, List<Feature>> hash,
+                                 String parent_id,
+                                 Set<Feature> children)
                throws InvalidRelationException
   {
-    Enumeration feature_enum = hash.elements();
+    Enumeration<List<Feature>> feature_enum = hash.elements();
     String parent;
     
     while(feature_enum.hasMoreElements())
     {
-      List child_list = (List)feature_enum.nextElement();
+      List<Feature> child_list = feature_enum.nextElement();
       
       for(int i=0; i<child_list.size(); i++)
       {       
-        Feature child = (Feature)child_list.get(i);
+        Feature child = child_list.get(i);
         //if(children.contains(child))
         //  continue;
         
@@ -330,8 +335,7 @@ public class ChadoCanonicalGene
           if(parent != null && parent.equals(parent_id))
             children.add(child);
         }
-      }
-      
+      } 
     }
   }
   
@@ -357,13 +361,13 @@ public class ChadoCanonicalGene
    * @throws InvalidRelationException
    */
   public void addSplicedFeatures(final String transcript_name, 
-                      final Feature spliced)
+                                 final Feature spliced)
   {   
-    final List v_spliced;
+    final List<Feature> v_spliced;
     if(splicedFeatures.containsKey(transcript_name))
-      v_spliced = (Vector)splicedFeatures.get(transcript_name);
+      v_spliced = (Vector<Feature>)splicedFeatures.get(transcript_name);
     else
-      v_spliced = new Vector();
+      v_spliced = new Vector<Feature>();
     
     v_spliced.add(spliced);
     splicedFeatures.put(transcript_name, v_spliced);
@@ -371,19 +375,19 @@ public class ChadoCanonicalGene
   
   public void correctSpliceSiteAssignments()
   {
-    Enumeration enumSplicedFeatures = splicedFeatures.keys();
+    Enumeration<String> enumSplicedFeatures = splicedFeatures.keys();
     while(enumSplicedFeatures.hasMoreElements())
     {
-      String transcriptId = (String)enumSplicedFeatures.nextElement();
-      Vector v_spliced = (Vector)splicedFeatures.get(transcriptId);
-      Set splicedTypes = getSpliceTypes(transcriptId);
-      Iterator it = splicedTypes.iterator();
+      String transcriptId = enumSplicedFeatures.nextElement();
+      Vector<Feature> v_spliced = (Vector<Feature>)splicedFeatures.get(transcriptId);
+      Set<String> splicedTypes = getSpliceTypes(transcriptId);
+      Iterator<String> it = splicedTypes.iterator();
       while(it.hasNext())
       {
-        String type = (String)it.next();
+        String type = it.next();
         if(!type.equals(DatabaseDocument.EXONMODEL))
         {
-          List splicedFeatures = getSpliceSitesOfTranscript(transcriptId, type);
+          List<Feature> splicedFeatures = getSpliceSitesOfTranscript(transcriptId, type);
           if(splicedFeatures.size() == 1)
           {
             Feature f = (Feature)splicedFeatures.get(0);
@@ -423,11 +427,11 @@ public class ChadoCanonicalGene
   public void add3PrimeUtr(final String transcript_name, 
                            final Feature utr) 
   {  
-    final List utr_list;
+    final List<Feature> utr_list;
     if(three_prime_utr.containsKey(transcript_name))
-      utr_list = (Vector)three_prime_utr.get(transcript_name);
+      utr_list = three_prime_utr.get(transcript_name);
     else
-      utr_list = new Vector();
+      utr_list = new Vector<Feature>();
     
     utr_list.add(utr);
     three_prime_utr.put(transcript_name, utr_list);
@@ -442,11 +446,11 @@ public class ChadoCanonicalGene
   public void add5PrimeUtr(final String transcript_name, 
                            final Feature utr)
   {
-    final List utr_list;
+    final List<Feature> utr_list;
     if(five_prime_utr.containsKey(transcript_name))
-      utr_list = (Vector)five_prime_utr.get(transcript_name);
+      utr_list = (Vector<Feature>)five_prime_utr.get(transcript_name);
     else
-      utr_list = new Vector();
+      utr_list = new Vector<Feature>();
     
     utr_list.add(utr);
     five_prime_utr.put(transcript_name, utr_list);
@@ -461,11 +465,11 @@ public class ChadoCanonicalGene
   public void addOtherFeatures(final String transcript_name, 
                                final Feature other_feature)
   {
-    final List v_other_features;
+    final List<Feature> v_other_features;
     if(other_features.containsKey(transcript_name))
-      v_other_features = (Vector)other_features.get(transcript_name);
+      v_other_features = (Vector<Feature>)other_features.get(transcript_name);
     else
-      v_other_features = new Vector();
+      v_other_features = new Vector<Feature>();
     v_other_features.add(other_feature);
     other_features.put(transcript_name, v_other_features);
   }
@@ -497,13 +501,13 @@ public class ChadoCanonicalGene
   }
  
   
-  public List getSpliceSitesOfTranscript(final String transcript_name,
+  public List<Feature> getSpliceSitesOfTranscript(final String transcript_name,
                                          final String type)
   {
     if(splicedFeatures.containsKey(transcript_name))
     {
-      List splicedFeaturesOfTranscript = (List)splicedFeatures.get(transcript_name);
-      List results = new Vector();
+      List<Feature> splicedFeaturesOfTranscript = splicedFeatures.get(transcript_name);
+      List<Feature> results = new Vector<Feature>();
       for(int i=0; i<splicedFeaturesOfTranscript.size(); i++)
       {
         Feature feature = (Feature)splicedFeaturesOfTranscript.get(i);
@@ -521,12 +525,12 @@ public class ChadoCanonicalGene
    * @param transcript_name
    * @return
    */
-  public Set getSpliceTypes(final String transcript_name)
+  public Set<String> getSpliceTypes(final String transcript_name)
   {
     if(splicedFeatures.containsKey(transcript_name))
     {
-      List splicedFeaturesOfTranscript = (List)splicedFeatures.get(transcript_name);
-      Set splicedTypes = new HashSet();
+      List<Feature> splicedFeaturesOfTranscript = splicedFeatures.get(transcript_name);
+      Set<String> splicedTypes = new HashSet<String>();
       for(int i=0; i<splicedFeaturesOfTranscript.size(); i++)
       {
         Feature feature = (Feature)splicedFeaturesOfTranscript.get(i);
@@ -543,11 +547,11 @@ public class ChadoCanonicalGene
    * @param transcript_name
    * @return
    */
-  public List getSplicedFeaturesOfTranscript(final String transcript_name)
+  public List<Feature> getSplicedFeaturesOfTranscript(final String transcript_name)
   {
     if(splicedFeatures.containsKey(transcript_name))
     {
-      return (List)splicedFeatures.get(transcript_name);
+      return splicedFeatures.get(transcript_name);
     }
  
     return null;
@@ -593,7 +597,7 @@ public class ChadoCanonicalGene
       return name;
 
     // check exons
-    List transcriptNames = getTranscriptNames();
+    List<String> transcriptNames = getTranscriptNames();
     feature = getSplicedFeatures(name);
     
     if(feature != null)
@@ -601,13 +605,13 @@ public class ChadoCanonicalGene
       for(int i=0; i<transcriptNames.size(); i++)
       {
         String transcriptName = (String)transcriptNames.get(i);
-        List splicedSegments = getSplicedFeaturesOfTranscript(transcriptName);
+        List<Feature> splicedSegments = getSplicedFeaturesOfTranscript(transcriptName);
         
         if(splicedSegments != null)
         {
           for(int j=0; j<splicedSegments.size(); j++)
           {
-            Feature segment = (Feature)splicedSegments.get(j);
+            Feature segment = splicedSegments.get(j);
             try
             {
               String segmentName = (String)segment.getQualifierByName("ID").getValues().get(0);
@@ -667,10 +671,10 @@ public class ChadoCanonicalGene
    * @param transcript_name
    * @return
    */
-  public List get3UtrOfTranscript(final String transcript_name)
+  public List<Feature> get3UtrOfTranscript(final String transcript_name)
   {
     if(three_prime_utr.containsKey(transcript_name))
-      return (List)three_prime_utr.get(transcript_name);
+      return (List<Feature>)three_prime_utr.get(transcript_name);
  
     return null;
   }
@@ -680,10 +684,10 @@ public class ChadoCanonicalGene
    * @param transcript_name
    * @return
    */
-  public List get5UtrOfTranscript(final String transcript_name)
+  public List<Feature> get5UtrOfTranscript(final String transcript_name)
   {
     if(five_prime_utr.containsKey(transcript_name))
-      return (List)five_prime_utr.get(transcript_name);
+      return (List<Feature>)five_prime_utr.get(transcript_name);
  
     return null;
   }
@@ -693,10 +697,10 @@ public class ChadoCanonicalGene
    * @param transcript_name
    * @return
    */
-  public List getOtherFeaturesOfTranscript(final String transcript_name)
+  public List<Feature> getOtherFeaturesOfTranscript(final String transcript_name)
   {
     if(other_features.containsKey(transcript_name))
-      return (List)other_features.get(transcript_name);
+      return other_features.get(transcript_name);
     return null;
   }
   
@@ -704,7 +708,7 @@ public class ChadoCanonicalGene
    * Get a list of trancripts.
    * @return
    */
-  public List getTranscripts()
+  public List<Feature> getTranscripts()
   {
     return transcripts;
   }
@@ -714,9 +718,9 @@ public class ChadoCanonicalGene
    * Get a list of trancripts.
    * @return
    */
-  private List getTranscriptNames()
+  private List<String> getTranscriptNames()
   {
-    List names = new Vector();
+    List<String> names = new Vector<String>();
     for(int i=0; i<transcripts.size(); i++)
     {
       Feature f = (Feature)transcripts.get(i);
@@ -756,10 +760,10 @@ public class ChadoCanonicalGene
       if(getFeatureFromHash(name, other_features) != null)
         return false;
       
-      final Enumeration enum_pp = proteins.elements();
+      final Enumeration<Feature> enum_pp = proteins.elements();
       while(enum_pp.hasMoreElements())
       {
-        final Feature pp = (Feature)enum_pp.nextElement();
+        final Feature pp = enum_pp.nextElement();
         if( getQualifier(pp, "ID").equals(name) )
           return false;
       }  
@@ -807,8 +811,8 @@ public class ChadoCanonicalGene
    */
   private boolean isSplicedFeatures(final String feature_id)
   {
-    List splicedFeatures = new Vector();
-    List transcripts = getTranscripts();
+    List<Feature> splicedFeatures = new Vector<Feature>();
+    List<Feature> transcripts = getTranscripts();
     
     try
     {
@@ -816,7 +820,7 @@ public class ChadoCanonicalGene
       {
         Feature transcript = (Feature) transcripts.get(i);
         String transcript_id = getQualifier(transcript, "ID");
-        List splicedSites = getSplicedFeaturesOfTranscript(transcript_id);
+        List<Feature> splicedSites = getSplicedFeaturesOfTranscript(transcript_id);
         if(splicedSites != null)
           splicedFeatures.addAll(splicedSites);
       }
@@ -1075,12 +1079,12 @@ public class ChadoCanonicalGene
    */
   private Feature getSplicedFeatures(final String name)
   {
-    Enumeration enum_exons = splicedFeatures.elements();
+    Enumeration<List<Feature>> enum_exons = splicedFeatures.elements();
     try
     {
       while(enum_exons.hasMoreElements())
       {
-        Vector exons = (Vector)enum_exons.nextElement();
+        List<Feature> exons = enum_exons.nextElement();
         
         for(int i=0; i<exons.size(); i++)
         {
@@ -1100,12 +1104,12 @@ public class ChadoCanonicalGene
   
   private Feature getProtein(final String id)
   {
-    Enumeration enum_proteins = proteins.elements();
+    Enumeration<Feature> enum_proteins = proteins.elements();
     try
     {
       while(enum_proteins.hasMoreElements())
       {
-        Feature protein = (Feature)enum_proteins.nextElement();
+        Feature protein = enum_proteins.nextElement();
         if(getQualifier(protein, "ID").equals(id))
           return protein;
       }
@@ -1126,18 +1130,18 @@ public class ChadoCanonicalGene
    */
   private Feature getFeatureFromHash
                          (final String id,
-                         final Hashtable UTR) 
+                         final Hashtable<String, List<Feature>> UTR) 
           throws InvalidRelationException
   {
-    Enumeration enum_utr = UTR.elements();
+    Enumeration<List<Feature>> enum_utr = UTR.elements();
 
     while(enum_utr.hasMoreElements())
     {
-      List utrs = (List)enum_utr.nextElement();
+      List<Feature> utrs = enum_utr.nextElement();
       
       for(int i=0; i<utrs.size(); i++)
       {
-        Feature utr = (Feature)utrs.get(i);
+        Feature utr = utrs.get(i);
         if(getQualifier(utr, "ID").equals(id))
           return utr;
       }
@@ -1192,7 +1196,7 @@ public class ChadoCanonicalGene
     this.srcfeature_id = srcfeature_id;
   }
 
-  public Hashtable getSplicedFeatures()
+  public Hashtable<String, List<Feature>> getSplicedFeatures()
   {
     return splicedFeatures;
   }
@@ -1211,11 +1215,11 @@ public class ChadoCanonicalGene
   {
     String transcriptName = getTranscriptFromName(
                          GeneUtils.getUniqueName(proteinFeature));
-    List spliced = getSplicedFeaturesOfTranscript(transcriptName);
+    List<Feature> spliced = getSplicedFeaturesOfTranscript(transcriptName);
     RangeVector ranges = new RangeVector();
     for(int i=0; i<spliced.size(); i++)
     {
-      Feature f = (Feature) spliced.get(i);
+      Feature f = spliced.get(i);
       if(f.getKey().getKeyString().equals(DatabaseDocument.EXONMODEL))
         ranges.addAll(f.getLocation().getRanges());
     }
@@ -1248,12 +1252,12 @@ public class ChadoCanonicalGene
   }
   
   
-  class RangeComparator implements Comparator
+  class RangeComparator implements Comparator<Range>
   {
-    public int compare(Object o1, Object o2)
+    public int compare(Range o1, Range o2)
     {
-      int start1 = ((Range)o1).getStart();
-      int start2 = ((Range)o2).getStart();
+      int start1 = o1.getStart();
+      int start2 = o2.getStart();
       return start1-start2;
     }
   }
