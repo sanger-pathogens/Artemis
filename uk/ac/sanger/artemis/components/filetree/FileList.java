5b195b6dc127d0254b778f478f5c1a8beda83fd7
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-04-26 15:25:54 +0000

    add getZipEntryContents
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@13747 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/components/filetree/FileList.java
+++ uk/ac/sanger/artemis/components/filetree/FileList.java
@@ -21,11 +21,16 @@
 
 package uk.ac.sanger.artemis.components.filetree;
 
+import uk.ac.sanger.artemis.components.ViewMenu;
 import uk.ac.sanger.artemis.j2ssh.FTProgress;
+import uk.ac.sanger.artemis.j2ssh.FileTransferProgressMonitor;
 import uk.ac.sanger.artemis.j2ssh.SshFileManager;
+import uk.ac.sanger.artemis.j2ssh.SshPSUClient;
+
 import java.util.Hashtable;
 import java.io.IOException;
 import java.io.File;
+
 import com.sshtools.j2ssh.sftp.FileAttributes;
 
 public class FileList 
@@ -139,6 +144,44 @@ public class FileList
   public byte[] getFileContents(String file, FTProgress monitor)
   {
     return ssh_client.getFileContents(file, monitor);
+  }
+  
+  public File getZipEntryContents(String zipFile, String entry, File dir_name)
+  {  
+    String zipDir = zipFile.substring(0, zipFile.lastIndexOf("/"));
+    String cmd = "unzip "+zipFile+" "+entry+" "+entry+".gz -d "+zipDir;
+    
+    SshPSUClient sshClient = new SshPSUClient(cmd);
+    sshClient.start();
+    FileAttributes attr = null;
+    
+    int count = 0;
+    while(count < 100 && (attr == null || !attr.isFile()))
+    {
+      attr = stat(zipDir+"/"+entry);
+      if(attr == null || !attr.isFile())
+      {
+        attr = stat(zipDir+"/"+entry+".gz");
+        if(attr != null && attr.isFile())
+          entry = entry+".gz";
+      }
+      count++;
+      try
+      {
+        Thread.sleep(200);
+      }
+      catch (InterruptedException e){}
+    }
+    
+    File localfn = new File(dir_name.getAbsoluteFile(), entry);
+    FileTransferProgressMonitor monitor = new FileTransferProgressMonitor(null);
+    FTProgress progress = monitor.add(localfn.getName());
+    
+    byte[] contents = getFileContents(zipDir+"/"+entry, progress);
+    delete(zipDir+"/"+entry);
+    ViewMenu.writeByteFile(contents, localfn);
+    monitor.close();
+    return localfn;
   }
 
 
