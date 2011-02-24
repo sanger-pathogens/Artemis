1a4ca9aa73e249c1b01b27b60e547a1fde87811e
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2011-02-24 16:27:13 +0000

    tidy
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/artemis/trunk@15610 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- uk/ac/sanger/artemis/components/variant/GraphPanel.java
+++ uk/ac/sanger/artemis/components/variant/GraphPanel.java
@@ -1,3 +1,25 @@
+/*
+ * created: 2011
+ *
+ * This file is part of Artemis
+ *
+ * Copyright(C) 2011  Genome Research Limited
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or(at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
 package uk.ac.sanger.artemis.components.variant;
 
 import java.awt.BasicStroke;
@@ -26,12 +48,9 @@ import javax.swing.JTextField;
 import uk.ac.sanger.artemis.FeatureVector;
 import uk.ac.sanger.artemis.components.variant.BCFReader.BCFReaderIterator;
 
-
 public class GraphPanel extends JPanel
 {
-  private static final long serialVersionUID = 1L;
-
-  
+  private static final long serialVersionUID = 1L; 
   protected boolean autoWinSize = true;
   protected int userWinSize = 1;
   protected JPopupMenu popup = new JPopupMenu();
