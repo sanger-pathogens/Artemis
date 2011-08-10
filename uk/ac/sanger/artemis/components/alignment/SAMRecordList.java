c8cc46e22b4c8996f0ec9d29e1dd60f8d296d8ca
tcarver <tjc>
2011-08-10 15:50:56 +0100

    additions to sam record listing

--- uk/ac/sanger/artemis/components/alignment/SAMRecordList.java
+++ uk/ac/sanger/artemis/components/alignment/SAMRecordList.java
@@ -24,6 +24,7 @@ package uk.ac.sanger.artemis.components.alignment;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
@@ -31,8 +32,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -41,24 +46,34 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 
+import uk.ac.sanger.artemis.Options;
+import uk.ac.sanger.artemis.components.DisplayAdjustmentEvent;
+import uk.ac.sanger.artemis.components.DisplayAdjustmentListener;
+import uk.ac.sanger.artemis.components.SwingWorker;
+
 
 import net.sf.samtools.SAMRecord;
 
 public class SAMRecordList extends JPanel
+                           implements DisplayAdjustmentListener
 {
   private static final long serialVersionUID = 1L;
   private BamView bamView;
   private JScrollBar verticalScroll = new JScrollBar(JScrollBar.VERTICAL);
-
+  private int font_size =12;
+  private int highlight = -1;
+  
   public SAMRecordList(final BamView bamView)
   {
     this.bamView = bamView;
     setBackground(Color.white);
 
+    font_size = Options.getOptions().getFont().getSize();
+
     final JFrame f = new JFrame("Reads");
     JPanel mainPanel = (JPanel) f.getContentPane();
     JPanel header = new JPanel(new FlowLayout(FlowLayout.LEADING));
-    header.add(new JLabel("NAME :: COORDINATES :: LENGTH :: QUALITY"));
+    header.add(new JLabel("NAME :: COORDINATES :: LENGTH :: QUALITY :: DIRECTION"));
     JButton close = new JButton("CLOSE");
     
     mainPanel.add(verticalScroll, BorderLayout.EAST);
@@ -66,10 +81,17 @@ public class SAMRecordList extends JPanel
     mainPanel.add(header, BorderLayout.NORTH);
     mainPanel.add(close, BorderLayout.SOUTH);
     
-    setPreferredSize(new Dimension(400,500));
+    setPreferredSize(new Dimension(450,500));
     f.pack();
     f.setVisible(true);
-    f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    f.addWindowListener(new WindowAdapter()
+    {
+      public void windowClosed(WindowEvent e)
+      {
+        windowClose(f);
+      }
+    });
     
     verticalScroll.addAdjustmentListener(new AdjustmentListener(){
       public void adjustmentValueChanged(AdjustmentEvent arg0)
@@ -82,32 +104,80 @@ public class SAMRecordList extends JPanel
     {
       public void mouseClicked(MouseEvent e)
       {
-        int highlight = e.getY()/12 + verticalScroll.getValue();
-        if(highlight > bamView.getReadsInView().size())
+        highlight = e.getY()/font_size + verticalScroll.getValue();
+        if(highlight >= bamView.getReadsInView().size())
           return;
 
-        bamView.setHighlightSAMRecord( bamView.getReadsInView().get(highlight) );
-        bamView.repaint();
-        repaint();
+        setHighlight();
+        if(e.getClickCount() > 1)
+        {
+          setCursor(new Cursor(Cursor.WAIT_CURSOR));
+          BamView.openFileViewer(bamView.getHighlightSAMRecord(),
+            bamView.getMate(bamView.getHighlightSAMRecord()));
+          setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+        }
       }
     });
+
+    setFocusable(true);
+    addKeyListener(new KeyAdapter()
+    {      
+      public void keyPressed(KeyEvent e)
+      {
+        int keyCode = e.getKeyCode();
+        switch( keyCode ) { 
+          case KeyEvent.VK_UP:
+            if(highlight == 0)
+              break;
+            highlight--;
+            setHighlight();
+            
+            if(highlight < verticalScroll.getValue())
+              verticalScroll.setValue(highlight);
+            break;
+          case KeyEvent.VK_DOWN:
+            if(highlight >= bamView.getReadsInView().size()-1)
+              break;
+            highlight++;
+            setHighlight();
+            
+            if(highlight > verticalScroll.getValue()+verticalScroll.getVisibleAmount())
+              verticalScroll.setValue(highlight);
+            break;
+        }
+
+      }
+    });
+
     
     close.addActionListener(new ActionListener()
     {
       public void actionPerformed(ActionEvent arg0)
       {
-        f.dispose();
+        windowClose(f);
       }
     });
+    
+    if(bamView.getFeatureDisplay() != null)
+      bamView.getFeatureDisplay().addDisplayAdjustmentListener(this);
+  }
+  
+  
+  private void setHighlight()
+  {
+    bamView.setHighlightSAMRecord( bamView.getReadsInView().get(highlight) );
+    bamView.repaint();
+    repaint();
   }
 
   protected void paintComponent(Graphics g)
   {
     super.paintComponent(g);
 
-    int nrecordsShown = getHeight()/12;
+    int nrecordsShown = getHeight()/font_size;
     List<SAMRecord> readsInView = bamView.getReadsInView();
-    verticalScroll.setMaximum(readsInView.size()-nrecordsShown);
+    verticalScroll.setMaximum(readsInView.size());
+    verticalScroll.setVisibleAmount(nrecordsShown);
 
     int fst = verticalScroll.getValue();
     int lst = nrecordsShown+fst+1;
@@ -126,7 +196,7 @@ public class SAMRecordList extends JPanel
       if(highlightedSAMRecord != null && highlightedSAMRecord.equals(thisRead.getReadName()))
       {
         g.setColor(Color.pink);
-        g.fillRect(0, ((i-fst)*12)+1, getWidth(), 12);
+        g.fillRect(0, ((i-fst)*font_size)+1, getWidth(), font_size);
         g.setColor(Color.black);
       }
       
@@ -135,8 +205,8 @@ public class SAMRecordList extends JPanel
                        thisRead.getAlignmentStart()+".."+thisRead.getAlignmentEnd(),
                        thisRead.getReadLength(),
                        thisRead.getMappingQuality(),
-                       (thisRead.getReadNegativeStrandFlag() ? "<--" : "-->"));
-      g.drawString(readStr, 5, (((i-fst)*12)+12));
+                       (thisRead.getReadNegativeStrandFlag() ? "-" : "+"));
+      g.drawString(readStr, 5, (((i-fst)*font_size)+font_size));
     }
   }
   
@@ -144,8 +214,8 @@ public class SAMRecordList extends JPanel
   {
     int nameWidth  = 10;
     int coordWidth = 10;
-    int lenWidth   = 7;
-    int qualWidth  = 7;
+    int lenWidth   = 4;
+    int qualWidth  = 4;
     
     for(int i=fst; i<lst; i++)
     {
@@ -169,6 +239,32 @@ public class SAMRecordList extends JPanel
 
     nameWidth++;
     coordWidth++;
-    return "%1$-"+nameWidth+"s| %2$-"+coordWidth+"s| %3$-"+lenWidth+"s| %4$-"+qualWidth+"s| %5$-4s";
+    return "%1$-"+nameWidth+"s| %2$-"+coordWidth+"s| %3$-"+lenWidth+"s| %4$-"+qualWidth+"s| %5$-2s";
+  }
+
+  public void displayAdjustmentValueChanged(DisplayAdjustmentEvent event)
+  {
+    SwingWorker worker = new SwingWorker()
+    {
+      public Object construct()
+      {
+        try
+        {
+          Thread.sleep(1500);
+        }
+        catch (InterruptedException e){}
+        
+        repaint();
+        return null;
+      }
+    };
+    worker.start();
+  }
+  
+  private void windowClose(JFrame f)
+  {
+    f.dispose();
+    if(bamView.getFeatureDisplay() != null)
+      bamView.getFeatureDisplay().removeDisplayAdjustmentListener(SAMRecordList.this);
   }
 }
